bf9c974def537cbbc52ac3010149cd496910f508
Enrico Weigelt, metux IT consult <info@metux.net>
2024-04-18 16:05:55 +0200

    WIP: drop symbol COMPOSITE
    FIXME: needs Xnest fixes merged first

--- hw/xfree86/common/xf86Helper.c
+++ hw/xfree86/common/xf86Helper.c
@@ -1497,11 +1497,9 @@ xf86SetBackingStore(ScreenPtr pScreen)
     else {
         if (xf86GetOptValBool(options, OPTION_BACKING_STORE, &useBS))
             from = X_CONFIG;
-#ifdef COMPOSITE
         if (from != X_CONFIG)
             useBS = xf86ReturnOptValBool(options, OPTION_BACKING_STORE,
                                          !noCompositeExtension);
-#endif
     }
     free(options);
     pScreen->backingStoreSupport = useBS ? WhenMapped : NotUseful;
