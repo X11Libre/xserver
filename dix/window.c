e8ab61e36cbcc62f4265eb9c199e1b2e6268c1cd
Enrico Weigelt, metux IT consult <info@metux.net>
2024-06-28 01:18:13 +0200

    (submit/selection.h) dix: rename selection.h to selection_priv.h
    rename it in order to reflects it's private nature (not exported).
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/window.c
+++ dix/window.c
@@ -106,6 +106,7 @@ Equipment Corporation.
 #include "dix/input_priv.h"
 #include "dix/property_priv.h"
 #include "dix/resource_priv.h"
+#include "dix/selection_priv.h"
 #include "mi/mi_priv.h"         /* miPaintWindow */
 #include "os/auth.h"
 #include "os/client_priv.h"
@@ -132,7 +133,6 @@ Equipment Corporation.
 #endif /* XINERAMA */
 #include "globals.h"
 #include "compint.h"
-#include "selection.h"
 #include "inpututils.h"
 #include "privates.h"
 #include "xace.h"
