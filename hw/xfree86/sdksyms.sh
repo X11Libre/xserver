ea69aadf4e47e4a97ebea9478a32cae9163055d8
Enrico Weigelt, metux IT consult <info@metux.net>
2024-06-28 00:44:38 +0200

    (submit/selection.h) dix: don't install selection.h anymore
    This file isn't included by any driver - not even indirectly, and hard
    to imagine any driver ever needs it, so no need to keep it installed.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/sdksyms.sh
+++ hw/xfree86/sdksyms.sh
@@ -260,7 +260,6 @@ cat > sdksyms.c << EOF
 #include "rgb.h"
 #include "screenint.h"
 #include "scrnintstr.h"
-#include "selection.h"
 #include "servermd.h"
 #include "validate.h"
 #include "window.h"
