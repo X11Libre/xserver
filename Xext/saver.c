d0159bce03defeab19c073b690da0435fb02b9e2
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-02 21:55:36 +0200

    Xext: saver: use REPLY_*() macros for preparing / sending replies
    Use the new macros for preparing and sending replies to clients.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/saver.c
+++ Xext/saver.c
@@ -598,11 +598,8 @@ ProcScreenSaverQueryVersion(ClientPtr client)
         .minorVersion = SERVER_SAVER_MINOR_VERSION
     };
 
-    if (client->swapped) {
-        swaps(&reply.majorVersion);
-        swaps(&reply.minorVersion);
-    }
-
+    REPLY_FIELD_CARD16(majorVersion);
+    REPLY_FIELD_CARD16(minorVersion);
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
 
@@ -633,8 +630,11 @@ ProcScreenSaverQueryInfo(ClientPtr client)
     lastInput = GetTimeInMillis() - LastEventTime(XIAllDevices).milliseconds;
 
     xScreenSaverQueryInfoReply reply = {
-        .window = pSaver->wid
+        .window = pSaver->wid,
+        .idle = lastInput,
+        .eventMask = getEventMask(pDraw->pScreen, client),
     };
+
     if (screenIsSaved != SCREEN_SAVER_OFF) {
         reply.state = ScreenSaverOn;
         if (ScreenSaverTime)
@@ -650,20 +650,18 @@ ProcScreenSaverQueryInfo(ClientPtr client)
             reply.state = ScreenSaverDisabled;
         }
     }
-    reply.idle = lastInput;
-    reply.eventMask = getEventMask(pDraw->pScreen, client);
+
     if (pPriv && pPriv->attr)
         reply.kind = ScreenSaverExternal;
     else if (ScreenSaverBlanking != DontPreferBlanking)
         reply.kind = ScreenSaverBlanked;
     else
         reply.kind = ScreenSaverInternal;
-    if (client->swapped) {
-        swapl(&reply.window);
-        swapl(&reply.tilOrSince);
-        swapl(&reply.idle);
-        swapl(&reply.eventMask);
-    }
+
+    REPLY_FIELD_CARD32(window);
+    REPLY_FIELD_CARD32(tilOrSince);
+    REPLY_FIELD_CARD32(idle);
+    REPLY_FIELD_CARD32(eventMask);
 
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
