a8ea2ee5909e04613a0a4e9b279d65a5aeb2c85c
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-08 17:27:02 +0200

    Xext: selinux: use REPLY_*() macros for preparing / sending replies
    Use the new macros for preparing and sending replies to clients.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xselinux_ext.c
+++ Xext/xselinux_ext.c
@@ -72,10 +72,9 @@ ProcSELinuxQueryVersion(ClientPtr client)
         .server_major = SELINUX_MAJOR_VERSION,
         .server_minor = SELINUX_MINOR_VERSION
     };
-    if (client->swapped) {
-        swaps(&reply.server_major);
-        swaps(&reply.server_minor);
-    }
+
+    REPLY_FIELD_CARD16(server_major);
+    REPLY_FIELD_CARD16(server_minor);
     X_SEND_REPLY_SIMPLE(client, reply);
     return Success;
 }
@@ -93,20 +92,11 @@ SELinuxSendContextReply(ClientPtr client, security_id_t sid)
     }
 
     SELinuxGetContextReply reply = {
-        .type = X_Reply,
-        .sequenceNumber = client->sequence,
-        .length = bytes_to_int32(len),
         .context_len = len
     };
 
-    if (client->swapped) {
-        swapl(&reply.length);
-        swaps(&reply.sequenceNumber);
-        swapl(&reply.context_len);
-    }
-
-    WriteToClient(client, sizeof(SELinuxGetContextReply), &reply);
-    WriteToClient(client, len, ctx);
+    REPLY_FIELD_CARD32(context_len);
+    REPLY_SEND_EXTRA(ctx, len);
     freecon(ctx);
     return Success;
 }
@@ -360,18 +350,15 @@ SELinuxSendItemsToClient(ClientPtr client, SELinuxListItemRec * items,
     /* Fill in the buffer */
     for (k = 0; k < count; k++) {
         buf[pos] = items[k].id;
-        if (client->swapped)
-            swapl(buf + pos);
+        REPLY_BUF_CARD32(buf + pos, 1);
         pos++;
 
         buf[pos] = items[k].octx_len * 4;
-        if (client->swapped)
-            swapl(buf + pos);
+        REPLY_BUF_CARD32(buf + pos, 1);
         pos++;
 
         buf[pos] = items[k].dctx_len * 4;
-        if (client->swapped)
-            swapl(buf + pos);
+        REPLY_BUF_CARD32(buf + pos, 1);
         pos++;
 
         memcpy((char *) (buf + pos), items[k].octx, strlen(items[k].octx) + 1);
@@ -383,20 +370,11 @@ SELinuxSendItemsToClient(ClientPtr client, SELinuxListItemRec * items,
 sendreply: ;
     /* Send reply to client */
     SELinuxListItemsReply reply = {
-        .type = X_Reply,
-        .sequenceNumber = client->sequence,
-        .length = size,
         .count = count
     };
 
-    if (client->swapped) {
-        swapl(&reply.length);
-        swaps(&reply.sequenceNumber);
-        swapl(&reply.count);
-    }
-
-    WriteToClient(client, sizeof(SELinuxListItemsReply), &reply);
-    WriteToClient(client, size * 4, buf);
+    REPLY_FIELD_CARD32(count);
+    REPLY_SEND_EXTRA(buf, size * sizeof(CARD32));
 
     /* Free stuff and return */
     rc = Success;
