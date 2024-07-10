27320738bbabb1c20f75334c591b43c81651698b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-10 16:49:54 +0200

    (submit/cleanup-vidmode-dispatch) Xext: vidmode: drop unnecessary if (client->swapped)
    The WriteSwappedDataToClient() already checks whether client is swapped
    and directly calls WriteToClient() if it's not.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/vidmode.c
+++ Xext/vidmode.c
@@ -1421,12 +1421,7 @@ ProcVidModeGetDotClocks(ClientPtr client)
     if (!ClockProg) {
         for (n = 0; n < numClocks; n++) {
             dotclock = Clocks[n];
-            if (client->swapped) {
-                WriteSwappedDataToClient(client, 4, (char *) &dotclock);
-            }
-            else {
-                WriteToClient(client, 4, &dotclock);
-            }
+            WriteSwappedDataToClient(client, 4, (char *) &dotclock);
         }
     }
 
