feed6459bd2eec5d39a4b997590a3c83f6713239
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-12 15:05:33 +0200

    xfixes: use REPLY_*() macros for preparing / sending replies
    Use the new macros for preparing and sending replies to clients.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- xfixes/cursor.c
+++ xfixes/cursor.c
@@ -384,15 +384,13 @@ ProcXFixesGetCursorImage(ClientPtr client)
         .cursorSerial = pCursor->serialNumber,
     };
 
-    if (client->swapped) {
-        swaps(&reply.x);
-        swaps(&reply.y);
-        swaps(&reply.width);
-        swaps(&reply.height);
-        swaps(&reply.xhot);
-        swaps(&reply.yhot);
-        swapl(&reply.cursorSerial);
-    }
+    REPLY_FIELD_CARD16(x);
+    REPLY_FIELD_CARD16(y);
+    REPLY_FIELD_CARD16(width);
+    REPLY_FIELD_CARD16(height);
+    REPLY_FIELD_CARD16(xhot);
+    REPLY_FIELD_CARD16(yhot);
+    REPLY_FIELD_CARD32(cursorSerial);
 
     return X_SEND_REPLY_WITH_RPCBUF(client, reply, rpcbuf);
 }
@@ -440,10 +438,9 @@ ProcXFixesGetCursorName(ClientPtr client)
         .atom = pCursor->name,
         .nbytes = strlen(str)
     };
-    if (client->swapped) {
-        swapl(&reply.atom);
-        swaps(&reply.nbytes);
-    }
+
+    REPLY_FIELD_CARD32(atom);
+    REPLY_FIELD_CARD16(nbytes);
 
     return X_SEND_REPLY_WITH_RPCBUF(client, reply, rpcbuf);
 }
@@ -499,17 +496,15 @@ ProcXFixesGetCursorImageAndName(ClientPtr client)
         .nbytes = strlen(name),
     };
 
-    if (client->swapped) {
-        swaps(&reply.x);
-        swaps(&reply.y);
-        swaps(&reply.width);
-        swaps(&reply.height);
-        swaps(&reply.xhot);
-        swaps(&reply.yhot);
-        swapl(&reply.cursorSerial);
-        swapl(&reply.cursorName);
-        swaps(&reply.nbytes);
-    }
+    REPLY_FIELD_CARD16(x);
+    REPLY_FIELD_CARD16(y);
+    REPLY_FIELD_CARD16(width);
+    REPLY_FIELD_CARD16(height);
+    REPLY_FIELD_CARD16(xhot);
+    REPLY_FIELD_CARD16(yhot);
+    REPLY_FIELD_CARD32(cursorSerial);
+    REPLY_FIELD_CARD32(cursorName);
+    REPLY_FIELD_CARD16(nbytes);
 
     return X_SEND_REPLY_WITH_RPCBUF(client, reply, rpcbuf);
 }
--- xfixes/region.c
+++ xfixes/region.c
@@ -435,14 +435,10 @@ ProcXFixesFetchRegion(ClientPtr client)
         .height = pExtent->y2 - pExtent->y1,
     };
 
-    if (client->swapped) {
-        swaps(&reply.sequenceNumber);
-        swapl(&reply.length);
-        swaps(&reply.x);
-        swaps(&reply.y);
-        swaps(&reply.width);
-        swaps(&reply.height);
-    }
+    REPLY_FIELD_CARD16(x);
+    REPLY_FIELD_CARD16(y);
+    REPLY_FIELD_CARD16(width);
+    REPLY_FIELD_CARD16(height);
 
     return X_SEND_REPLY_WITH_RPCBUF(client, reply, rpcbuf);
 }
