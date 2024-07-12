4b1b39121516c22fe854617aa946fbab94bd9f39
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-12 17:29:24 +0200

    Xext: xvmc: use REQUEST_HEAD_STRUCT and REQUEST_FIELD_* macros
    Use the new macros to make request struct parsing / field swapping
    much easier.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xvmc.c
+++ Xext/xvmc.c
@@ -110,10 +110,8 @@ ProcXvMCQueryVersion(ClientPtr client)
         .minor = SERVER_XVMC_MINOR_VERSION
     };
 
-    if (client->swapped) {
-        swapl(&reply.major);
-        swapl(&reply.minor);
-    }
+    X_REPLY_FIELD_CARD32(major);
+    X_REPLY_FIELD_CARD32(minor);
 
     return X_SEND_REPLY_SIMPLE(client, reply);
 }
