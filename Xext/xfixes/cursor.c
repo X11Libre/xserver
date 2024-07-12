802b7c3a5b4280f3a7c1498d3bf9543407e41a9c
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-12 12:45:55 +0200

    FIXME xfixes: use REQUEST_HEAD_STRUCT and REQUEST_FIELD_* macros
    Use the new macros to make request struct parsing / field swapping
    much easier.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xfixes/cursor.c
+++ Xext/xfixes/cursor.c
@@ -601,11 +601,11 @@ TestForCursor(CursorPtr pCursor, void *closure)
 int
 ProcXFixesChangeCursor(ClientPtr client)
 {
-    CursorPtr pSource, pDestination;
-
     X_REQUEST_HEAD_STRUCT(xXFixesChangeCursorReq);
     X_REQUEST_FIELD_CARD32(source);
     X_REQUEST_FIELD_CARD32(destination);
+
+    CursorPtr pSource, pDestination;
 
     VERIFY_CURSOR(pSource, stuff->source, client,
                   DixReadAccess | DixGetAttrAccess);
@@ -870,7 +870,6 @@ int
 ProcXFixesCreatePointerBarrier(ClientPtr client)
 {
     X_REQUEST_HEAD_AT_LEAST(xXFixesCreatePointerBarrierReq);
-
     X_REQUEST_FIELD_CARD16(num_devices);
     X_REQUEST_FIELD_CARD32(barrier);
     X_REQUEST_FIELD_CARD32(window);
