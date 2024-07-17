57157c9999e9f5f4613e8a51d2725a3e9cec097b
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-17 11:07:27 +0200

    dbe: ProcDbeGetVisualInfo() collect payload in buffer before writing
    The payload lengths is already known, so we can easily collect the data
    in a stack buffer and only need one WriteToClient() operation.
    
    This also clears the road for further simplification/unification of the
    reply sending code, coming with follow-up commits.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dbe/dbe.c
+++ dbe/dbe.c
@@ -626,6 +626,7 @@ ProcDbeGetVisualInfo(ClientPtr client)
 
         free(visualInfo.visinfo);
     }
+    WriteToClient(client, length, buf);
 
     if (rpcbuf.error) {
         rc = BadAlloc;
@@ -646,6 +647,7 @@ ProcDbeGetVisualInfo(ClientPtr client)
 clearRpcBuf:
     x_rpcbuf_clear(&rpcbuf);
     free(pDrawables);
+    free(buf);
 
     return rc;
 
