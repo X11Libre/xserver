33c2fc712e50bc2bc0cf39826e60bfe5f8ffe4ee
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-17 12:27:18 +0200

    Xext: selinux: use static reply struct init on declaration
    Make the code a bit easier to read by using initialization of the reply
    structs, at the point of declaration. Most of them aren't written to later,
    just passed into WriteReplyToClient(). Also dropping some useless zero
    assignments (struct initializers automatically zero-out unmentioned fields).
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xselinux_ext.c
+++ Xext/xselinux_ext.c
@@ -70,7 +70,6 @@ ProcSELinuxQueryVersion(ClientPtr client)
     SELinuxQueryVersionReply rep = {
         .type = X_Reply,
         .sequenceNumber = client->sequence,
-        .length = 0,
         .server_major = SELINUX_MAJOR_VERSION,
         .server_minor = SELINUX_MINOR_VERSION
     };
@@ -87,7 +86,6 @@ ProcSELinuxQueryVersion(ClientPtr client)
 static int
 SELinuxSendContextReply(ClientPtr client, security_id_t sid)
 {
-    SELinuxGetContextReply rep;
     char *ctx = NULL;
     int len = 0;
 
@@ -97,7 +95,7 @@ SELinuxSendContextReply(ClientPtr client, security_id_t sid)
         len = strlen(ctx) + 1;
     }
 
-    rep = (SELinuxGetContextReply) {
+    SELinuxGetContextReply rep = {
         .type = X_Reply,
         .sequenceNumber = client->sequence,
         .length = bytes_to_int32(len),
@@ -345,7 +343,6 @@ SELinuxSendItemsToClient(ClientPtr client, SELinuxListItemRec * items,
                          int size, int count)
 {
     int k, pos = 0;
-    SELinuxListItemsReply rep;
     CARD32 buf[size];
 
     /* Fill in the buffer */
@@ -372,7 +369,7 @@ SELinuxSendItemsToClient(ClientPtr client, SELinuxListItemRec * items,
     }
 
     /* Send reply to client */
-    rep = (SELinuxListItemsReply) {
+    SELinuxListItemsReply rep = {
         .type = X_Reply,
         .sequenceNumber = client->sequence,
         .length = size,
