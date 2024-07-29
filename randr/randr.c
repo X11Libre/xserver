197b1c647b05dba349771194f8d355ef26eb8575
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-29 14:22:00 +0200

    randr: use explicit case statement instead of ProcRandrVector table
    No need to go indirectly through a vector table, since everything's fixed
    anyways. It's not a pretty robust programming style: any changes need great
    care, in order to not mix up things.
    
    Replacing this by direct switch/case statement, which is using the defines
    from the xrandr protocol headers. Also adding a little bit more protection
    against subtle programming errors and reducing cognitive load (source size)
    on understanding the code by using a tiny macro for deducing define name and
    function name from the request's name.
    
    This approach actually uncovered some subtle bug that had been waiting in
    the dark for over 15 years.
    
    As collateral benefit, getting a tiny bit better performance.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- randr/randr.c
+++ randr/randr.c
@@ -48,7 +48,6 @@ static int RRNScreens;
     real->mem = priv->mem; \
 }
 
-static int ProcRRDispatch(ClientPtr pClient);
 static int SProcRRDispatch(ClientPtr pClient);
 
 int RREventBase;
@@ -719,16 +718,6 @@ RRVerticalRefresh(xRRModeInfo * mode)
     if (refresh > 0xffff)
         refresh = 0xffff;
     return (CARD16) refresh;
-}
-
-static int
-ProcRRDispatch(ClientPtr client)
-{
-    REQUEST(xReq);
-    if (stuff->data >= RRNumberRequests || !ProcRandrVector[stuff->data])
-        return BadRequest;
-    UpdateCurrentTimeIf();
-    return (*ProcRandrVector[stuff->data]) (client);
 }
 
 static int _X_COLD
