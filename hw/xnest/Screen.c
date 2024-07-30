fec61fcae79e08a53bf7f62fcfb6d1f91e2aada9
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-30 20:08:31 +0200

    Xnest: helper for fetching upstream screen's root window

--- hw/xnest/Screen.c
+++ hw/xnest/Screen.c
@@ -359,7 +359,7 @@ xnestOpenScreen(ScreenPtr pScreen, int argc, char *argv[])
         else
             xnestDefaultWindows[pScreen->myNum] =
                 XCreateWindow(xnestDisplay,
-                              DefaultRootWindow(xnestDisplay),
+                              xnestUpstreamRootWindow(),
                               xnestX + POSITION_OFFSET,
                               xnestY + POSITION_OFFSET,
                               xnestWidth, xnestHeight,
