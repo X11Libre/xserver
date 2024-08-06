b23c577e93d6db507934a0cdda4fa61e53324a94
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-06 14:48:11 +0200

    (submit/fixup-req-len) Xext: geext: drop swapping request length fields
    The request struct's length fields aren't used anymore - we have the
    client->req_len field instead, which also is bigreq-compatible.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/geext.c
+++ Xext/geext.c
@@ -101,8 +101,6 @@ static int _X_COLD
 SProcGEQueryVersion(ClientPtr client)
 {
     REQUEST(xGEQueryVersionReq);
-
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xGEQueryVersionReq);
     swaps(&stuff->majorVersion);
     swaps(&stuff->minorVersion);
