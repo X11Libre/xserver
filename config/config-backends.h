9d50dce43278c1ae2dc201d59c4b084827dcebd8
Enrico Weigelt, metux IT consult <info@metux.net>
2024-10-14 20:49:21 +0200

    2

--- config/config-backends.h
+++ config/config-backends.h
@@ -22,13 +22,9 @@
  *
  * Author: Daniel Stone <daniel@fooishbar.org>
  */
-
 #ifndef XSERVER_CONFIG_BACKENDS_H
 #define XSERVER_CONFIG_BACKENDS_H
 
-#ifdef HAVE_DIX_CONFIG_H
-#include <dix-config.h>
-#endif
 #include "input.h"
 #include "list.h"
 
