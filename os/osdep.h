metux/xorg-xserver

os: replace GenerateRandomData() by custom arc4random_buf() on platforms that missing it
arc4random_buf() is a pretty standard libc function on Unix'oid platforms,
but not all our targets have it, thus we need a fallback there. Currently we
have GenerateRandomData(), which either just wraps arc4random_buf() or provides
some fallback implementation.

For those cases it's easier to just implement missing functions directly
instead of having custom wrapper functions. So, drop GenerateRandomData()
in favor of arc4random_buf() and provide fallback implementation for where
it is missing.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

os/osdep.h
@@ -151,9 +151,6 @@ extern WorkQueuePtr workQueue;
 /* in access.c */
 extern Bool ComputeLocalClient(ClientPtr client);
 
-/* in auth.c */
-extern void GenerateRandomData(int len, char *buf);
-
 /* in mitauth.c */
 extern XID MitCheckCookie(AuthCheckArgs);
 extern XID MitGenerateCookie(AuthGenCArgs);
@@ -207,5 +204,17 @@ extern void XdmcpRegisterBroadcastAddress(const struct sockaddr_in *addr);
 #ifdef HASXDMAUTH
 extern void XdmAuthenticationInit(const char *cookie, int cookie_length);
 #endif
+
+/* for platforms lacking arc4random_buf() libc function */
+#ifndef HAVE_ARC4RANDOM_BUF
+static inline void arc4random_buf(char *buf, size_t n)
+{
+    int fd;
+
+    fd = open("/dev/urandom", O_RDONLY);
+    read(fd, buf, len);
+    close(fd);
+}
+#endif /* HAVE_ARC4RANDOM_BUF */
 
 #endif                          /* _OSDEP_H_ */

X11Libre/xserver

os/osdep.h
@@ -134,8 +134,17 @@ extern Bool NewOutputPending;
 /* in access.c */
 extern Bool ComputeLocalClient(ClientPtr client);
 
-/* in auth.c */
-extern void GenerateRandomData(int len, char *buf);
+/* for platforms lacking arc4random_buf() libc function */
+#ifndef HAVE_ARC4RANDOM_BUF
+static inline void arc4random_buf(char *buf, size_t len)
+{
+    int fd;
+
+    fd = open("/dev/urandom", O_RDONLY);
+    read(fd, buf, len);
+    close(fd);
+}
+#endif /* HAVE_ARC4RANDOM_BUF */
 
 /* OsTimer functions */
 void TimerInit(void);