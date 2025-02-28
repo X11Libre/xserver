92a2a846f9d6fe671b2b2507e7337b80ca1f4ccb
Enrico Weigelt, metux IT consult <info@metux.net>
2025-02-28 17:58:35 +0100

    xkb: make XkbFreeGeomSections() static
    Only used within one source file.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    (cherry picked from commit 220b86f119783dcccb367d5047f4c7a9d0f017b1)

--- Xext/xkeyboard/xkbgeom_priv.h
+++ Xext/xkeyboard/xkbgeom_priv.h
@@ -318,13 +318,6 @@ XkbDoodadPtr SrvXkbAddGeomDoodad(XkbGeometryPtr geom,
                                  Atom name);
 
 extern void
- XkbFreeGeomSections(XkbGeometryPtr /* geom */ ,
-                     int /* first */ ,
-                     int /* count */ ,
-                     Bool       /* freeAll */
-    );
-
-extern void
  XkbFreeGeomPoints(XkbOutlinePtr /* outline */ ,
                    int /* first */ ,
                    int /* count */ ,
