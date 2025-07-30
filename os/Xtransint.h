fb916fa0eb3aa2e9b27b68626e93be1a30f51043
Enrico Weigelt, metux IT consult <info@metux.net>
2025-07-30 14:30:08 +0200

    WIP: XTRANS_SEND_FDS

--- os/Xtransint.h
+++ os/Xtransint.h
@@ -100,15 +100,11 @@ from The Open Group.
 
 #define X_TCP_PORT	6000
 
-#if XTRANS_SEND_FDS
-
 struct _XtransConnFd {
     struct _XtransConnFd   *next;
     int                    fd;
     int                    do_close;
 };
-
-#endif
 
 struct _XtransConnInfo {
     struct _Xtransport     *transptr;
@@ -175,7 +171,6 @@ typedef struct _Xtransport {
 
     ssize_t (*Write)(XtransConnInfo ciptr, const char *buf, size_t size);
 
-#if XTRANS_SEND_FDS
     int (*SendFd)(
 	XtransConnInfo,		/* connection */
         int,                    /* fd */
@@ -185,7 +180,6 @@ typedef struct _Xtransport {
     int (*RecvFd)(
 	XtransConnInfo		/* connection */
     );
-#endif
 
     int	(*Disconnect)(
 	XtransConnInfo		/* connection */
