X11Libre/xserver

commit 1c13cfa6ca09c20c10b4233a56a02a7a7caeda07

os: io: factor out making room in output buffer
First step for simplifying the output path - this is really complicated now:

FlushClient() is called in two cases:

a) we really need to send out critical data (eg. critical events now),
   here we have no extra data
b) going to write new data in the output buffer, but it's already full
   here we do have extra data

In case b) (only called from WriteToClient()) we're first trying to write out
as much as we can, and if there's still not enough room, the buffer is resized.
The write-out path is a complex look trying to write buffered data first, then
the new data. That's even more complex since using writev() with 3 pieces
(old buffer, new data, padding), and considering each of those could be written
just partially.

By the way, there's really no need that the new data is strictly written
along with the already buffered one - practically that's not even any actual
performance optimization - so it's just making things unncessarily complicated.

Therefore reduce it to what's really needed: ensure enough room in the output
buffer (and potentially flush out or resize the buffer). In a later, remove the
whole extra data part from FlushClient(), as it's not needed anymore.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

os/io.c
@@ -60,6 +60,7 @@ SOFTWARE.
 #ifdef WIN32
 #include <X11/Xwinsock.h>
 #endif
+#include <stdbool.h>
 #include <stdio.h>
 #include "os/Xtrans.h"
 #include <X11/Xmd.h>
@@ -669,6 +670,47 @@ AbortClient(ClientPtr client)
     }
 }
 
+/*
+ * try to make room in the output buffer:
+ * if not enough room, try to flush first.
+ * if that's not giving enough room, increase the buffer size
+ */
+static bool OutputBufferMakeRoom(ClientPtr who, OsCommPtr oc, size_t sz)
+{
+    const size_t padsize = padding_for_int32(sz);
+    const size_t needed = sz + padsize;
+
+    ConnectionOutputPtr oco = oc->output;
+
+    /* check whether it already fits into buffer */
+    if (oco->count + needed <= oco->size)
+        return true;
+
+    /* try flushing the buffer */
+    int ret = FlushClient(who, oc, NULL, 0);
+    if (ret == -1) /* client was aborted */
+        return false;
+
+    /* does it fit this time ? */
+    if (oco->count + needed <= oco->size)
+        return true;
+
+    /* try to resize the buffer */
+    const int newsize = oco->count + (((needed / BUFSIZE)+1)*BUFSIZE);
+
+    void *newbuf = realloc(oco->buf, newsize);
+    if (!newbuf) {
+        AbortClient(who);
+        dixMarkClientException(who);
+        oco->count = 0;
+        return FALSE;
+    }
+
+    oco->buf = newbuf;
+    oco->size = newsize;
+    return true;
+}
+
 /*****************
  * WriteToClient
  *    Copies buf into ClientPtr.buf if it fits (with padding), else
@@ -799,9 +841,11 @@ WriteToClient(ClientPtr who, int count, const void *__buf)
             CriticalOutputPending = FALSE;
             NewOutputPending = FALSE;
         }
-
-        return FlushClient(who, oc, buf, count);
     }
+
+    /* if we fail to make room, the client will be aborted */
+    if (!OutputBufferMakeRoom(who, oc, count))
+        return -1;
 
     NewOutputPending = TRUE;
     output_pending_mark(who);

commit 9c059bde3a31c3df4774444417e1f5446c8c07fd

os: io: fix NULL output buffer after FlushClient()
FlushClient() does drops the output buffer, when it becomes empty.
This previously wasn't any problem and actually intented, because we've
returned from the write path directly after FlushClient() was called.

But now we've changed the call order, so it's called from within
OutputBufferMakeRoom(), so we need to make sure we always got valid
output buffer before trying to access it.

For the future, we should think about whether dropping output buffers
really makes sense at all. Instead we could leave them as they are
(over the client's lifetime) and maybe just trim when they've went
too big.

Fixes: 1c13cfa6ca09c20c10b4233a56a02a7a7caeda07
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

os/io.c
@@ -671,6 +671,27 @@ AbortClient(ClientPtr client)
 }
 
 /*
+ * make sure we have an output buffer in the OsComm
+ */
+static bool OutputEnsureBuffer(ClientPtr who, OsCommPtr oc)
+{
+    if (oc->output)
+        return true;
+
+    if ((oc->output = FreeOutputs)) {
+        FreeOutputs = oc->output->next;
+        return true;
+    }
+
+    if ((oc->output = AllocateOutputBuffer()))
+        return true;
+
+    AbortClient(who);
+    dixMarkClientException(who);
+    return false;
+}
+
+/*
  * try to make room in the output buffer:
  * if not enough room, try to flush first.
  * if that's not giving enough room, increase the buffer size
@@ -680,16 +701,21 @@ static bool OutputBufferMakeRoom(ClientPtr who, OsCommPtr oc, size_t sz)
     const size_t padsize = padding_for_int32(sz);
     const size_t needed = sz + padsize;
 
-    ConnectionOutputPtr oco = oc->output;
+    if (oc->output) {
+        /* check whether it already fits into buffer */
+        if (oc->output->count + needed <= oc->output->size)
+            return true;
 
-    /* check whether it already fits into buffer */
-    if (oco->count + needed <= oco->size)
-        return true;
+        /* try flushing the buffer */
+        int ret = FlushClient(who, oc);
+        if (ret == -1) /* client was aborted */
+            return false;
+    }
 
-    /* try flushing the buffer */
-    int ret = FlushClient(who, oc);
-    if (ret == -1) /* client was aborted */
+    if (!OutputEnsureBuffer(who, oc))
         return false;
+
+    ConnectionOutputPtr oco = oc->output;
 
     /* does it fit this time ? */
     if (oco->count + needed <= oco->size)
@@ -703,7 +729,7 @@ static bool OutputBufferMakeRoom(ClientPtr who, OsCommPtr oc, size_t sz)
         AbortClient(who);
         dixMarkClientException(who);
         oco->count = 0;
-        return FALSE;
+        return false;
     }
 
     oco->buf = newbuf;
@@ -726,7 +752,6 @@ int
 WriteToClient(ClientPtr who, int count, const void *__buf)
 {
     OsCommPtr oc;
-    ConnectionOutputPtr oco;
     int padBytes;
     const char *buf = __buf;
 
@@ -739,7 +764,6 @@ WriteToClient(ClientPtr who, int count, const void *__buf)
     if (!count || !who || who == serverClient || who->clientGone)
         return 0;
     oc = who->osPrivate;
-    oco = oc->output;
 #ifdef DEBUG_COMMUNICATION
     {
         char info[128];
@@ -780,18 +804,6 @@ WriteToClient(ClientPtr who, int count, const void *__buf)
             multicount = TRUE;
     }
 #endif
-
-    if (!oco) {
-        if ((oco = FreeOutputs)) {
-            FreeOutputs = oco->next;
-        }
-        else if (!(oco = AllocateOutputBuffer())) {
-            AbortClient(who);
-            dixMarkClientException(who);
-            return -1;
-        }
-        oc->output = oco;
-    }
 
     padBytes = padding_for_int32(count);
 
@@ -835,6 +847,12 @@ WriteToClient(ClientPtr who, int count, const void *__buf)
         }
     }
 #endif
+
+    if (!OutputEnsureBuffer(who, oc))
+        return -1;
+
+    ConnectionOutputPtr oco = oc->output;
+
     if ((oco->count == 0 && who->local) || oco->count + count + padBytes > oco->size) {
         output_pending_clear(who);
         if (!any_output_pending()) {
@@ -846,6 +864,8 @@ WriteToClient(ClientPtr who, int count, const void *__buf)
     /* if we fail to make room, the client will be aborted */
     if (!OutputBufferMakeRoom(who, oc, count))
         return -1;
+
+    oco = oc->output;
 
     NewOutputPending = TRUE;
     output_pending_mark(who);