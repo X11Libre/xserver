fe7f62ae716cfab441f88e4d50d3acb0ba5fa0e7
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-12 13:33:38 +0200

    Xinerama: lambda-esque macro for walking Xinerama screens
    Move the walking loops on Xinerama screens into lambda-esque macros:
    the callers look quite like we've been using lambda functions and
    closures, but actually are just fancy macro trickery.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xvdisp.c
+++ Xext/xvdisp.c
@@ -1258,14 +1258,13 @@ XineramaXvStopVideo(ClientPtr client)
     if (result != Success)
         return result;
 
-    unsigned int walkScreenIdx;
-    FOR_NSCREENS_BACKWARD(walkScreenIdx) {
+    XINERAMA_FOR_EACH_SCREEN_BACKWARD({
         if (port->info[walkScreenIdx].id) {
             stuff->drawable = draw->info[walkScreenIdx].id;
             stuff->port = port->info[walkScreenIdx].id;
             result = SingleXvStopVideo(client);
         }
-    }
+    });
 
     return result;
 }
@@ -1284,13 +1283,13 @@ XineramaXvSetPortAttribute(ClientPtr client)
     if (result != Success)
         return result;
 
-    unsigned int walkScreenIdx;
-    FOR_NSCREENS_BACKWARD(walkScreenIdx) {
+    XINERAMA_FOR_EACH_SCREEN_BACKWARD({
         if (port->info[walkScreenIdx].id) {
             stuff->port = port->info[walkScreenIdx].id;
             result = SingleXvSetPortAttribute(client);
         }
-    }
+    });
+
     return result;
 }
 
@@ -1328,10 +1327,7 @@ XineramaXvShmPutImage(ClientPtr client)
     x = stuff->drw_x;
     y = stuff->drw_y;
 
-    unsigned int walkScreenIdx;
-    FOR_NSCREENS_BACKWARD(walkScreenIdx) {
-        ScreenPtr walkScreen = screenInfo.screens[walkScreenIdx];
-
+    XINERAMA_FOR_EACH_SCREEN_BACKWARD({
         if (port->info[walkScreenIdx].id) {
             stuff->drawable = draw->info[walkScreenIdx].id;
             stuff->port = port->info[walkScreenIdx].id;
@@ -1346,7 +1342,8 @@ XineramaXvShmPutImage(ClientPtr client)
 
             result = SingleXvShmPutImage(client);
         }
-    }
+    });
+
     return result;
 }
 #else /* CONFIG_MITSHM */
@@ -1383,9 +1380,7 @@ XineramaXvPutImage(ClientPtr client)
     x = stuff->drw_x;
     y = stuff->drw_y;
 
-    unsigned int walkScreenIdx;
-    FOR_NSCREENS_BACKWARD(walkScreenIdx) {
-        ScreenPtr walkScreen = screenInfo.screens[walkScreenIdx];
+    XINERAMA_FOR_EACH_SCREEN_BACKWARD({
         if (port->info[walkScreenIdx].id) {
             stuff->drawable = draw->info[walkScreenIdx].id;
             stuff->port = port->info[walkScreenIdx].id;
@@ -1399,7 +1394,8 @@ XineramaXvPutImage(ClientPtr client)
 
             result = SingleXvPutImage(client);
         }
-    }
+    });
+
     return result;
 }
 
@@ -1433,9 +1429,7 @@ XineramaXvPutVideo(ClientPtr client)
     x = stuff->drw_x;
     y = stuff->drw_y;
 
-    unsigned int walkScreenIdx;
-    FOR_NSCREENS_BACKWARD(walkScreenIdx) {
-        ScreenPtr walkScreen = screenInfo.screens[walkScreenIdx];
+    XINERAMA_FOR_EACH_SCREEN_BACKWARD({
         if (port->info[walkScreenIdx].id) {
             stuff->drawable = draw->info[walkScreenIdx].id;
             stuff->port = port->info[walkScreenIdx].id;
@@ -1449,7 +1443,8 @@ XineramaXvPutVideo(ClientPtr client)
 
             result = SingleXvPutVideo(client);
         }
-    }
+    });
+
     return result;
 }
 
@@ -1483,9 +1478,7 @@ XineramaXvPutStill(ClientPtr client)
     x = stuff->drw_x;
     y = stuff->drw_y;
 
-    unsigned int walkScreenIdx;
-    FOR_NSCREENS_BACKWARD(walkScreenIdx) {
-        ScreenPtr walkScreen = screenInfo.screens[walkScreenIdx];
+    XINERAMA_FOR_EACH_SCREEN_BACKWARD({
         if (port->info[walkScreenIdx].id) {
             stuff->drawable = draw->info[walkScreenIdx].id;
             stuff->port = port->info[walkScreenIdx].id;
@@ -1496,10 +1489,10 @@ XineramaXvPutStill(ClientPtr client)
                 stuff->drw_x -= walkScreen->x;
                 stuff->drw_y -= walkScreen->y;
             }
-
             result = SingleXvPutStill(client);
         }
-    }
+    });
+
     return result;
 }
 
@@ -1584,13 +1577,9 @@ XineramifyXv(void)
         MatchingAdaptors[0] = refAdapt;
         isOverlay = hasOverlay(refAdapt);
 
-        unsigned int walkScreenIdx;
-        FOR_NSCREENS_FORWARD(walkScreenIdx) {
-            ScreenPtr walkScreen = screenInfo.screens[walkScreenIdx];
-            if (!walkScreenIdx)
-                continue; /* skip screen #0 */
+        XINERAMA_FOR_EACH_SCREEN_FORWARD_SKIP0({
             MatchingAdaptors[walkScreenIdx] = matchAdaptor(walkScreen, refAdapt, isOverlay);
-        }
+        });
 
         /* now create a resource for each port */
         for (int j = 0; j < refAdapt->nPorts; j++) {
@@ -1599,12 +1588,13 @@ XineramifyXv(void)
             if (!port)
                 break;
 
-            FOR_NSCREENS_BACKWARD(walkScreenIdx) {
+            XINERAMA_FOR_EACH_SCREEN_BACKWARD({
                 if (MatchingAdaptors[walkScreenIdx] && (MatchingAdaptors[walkScreenIdx]->nPorts > j))
                     port->info[walkScreenIdx].id = MatchingAdaptors[walkScreenIdx]->base_id + j;
                 else
                     port->info[walkScreenIdx].id = 0;
-            }
+            });
+
             AddResource(port->info[0].id, XvXRTPort, port);
         }
     }
