2eda615dca4b51df58446f805406731e7cc9d0be
Enrico Weigelt, metux IT consult <info@metux.net>
2025-08-26 12:05:21 +0200

    Xext: xv: don't try to use xvmc images anymore
    Since XvMC protocol has been removed, there's no way that XvMC images
    ever getting created anymore, so no point in trying to find/use one.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xvdisp.c
+++ Xext/xvdisp.c
@@ -726,10 +726,6 @@ ProcXvShmPutImage(ClientPtr client)
 #endif /* CONFIG_MITSHM */
 }
 
-#ifdef XvMCExtension
-#include "xvmcext.h"
-#endif
-
 __size_assert(int, sizeof(INT32));
 
 static int
@@ -752,11 +748,6 @@ ProcXvQueryImageAttributes(ClientPtr client)
             break;
         }
     }
-
-#ifdef XvMCExtension
-    if (!pImage)
-        pImage = XvMCFindXvImage(pPort, stuff->id);
-#endif
 
     if (!pImage)
         return BadMatch;
