pelya/xserver-xsdl

commit 9a26d6f39e199bad287d4e538ef75700a0102788

Bugzilla #1883 (https://freedesktop.org/bugzilla/show_bug.cgi?id=1883): Fix     insufficient SHM detection in XvMC local test. Removed reference to     getpagesize() which caused linking problems on s390. Reported by Stefan     Dirsch <sndirsh@suse.de>

Xext/xvmc.c
@@ -17,10 +17,21 @@
 #include "Xvproto.h"
 #include "XvMCproto.h"
 #include "xvmcext.h"
-#include "xf86_ansic.h"
+
+#ifdef HAS_XVMCSHM
+#ifndef Lynx
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#else
+#include <ipc.h>
+#include <shm.h>
+#endif /* Lynx */
+#endif /* HAS_XVMCSHM */
+   
+
 
 #define DR_CLIENT_DRIVER_NAME_SIZE 48
-#define DR_BUSID_SIZE 20
+#define DR_BUSID_SIZE 48
 
 int XvMCScreenIndex = -1;
 
@@ -558,7 +569,10 @@ ProcXvMCGetDRInfo(ClientPtr client)
     XvPortPtr pPort;
     ScreenPtr pScreen;
     XvMCScreenPtr pScreenPriv;
+
+#ifdef HAS_XVMCSHM
     volatile CARD32 *patternP;
+#endif
 
     REQUEST(xvmcGetDRInfoReq);
     REQUEST_SIZE_MATCH(xvmcGetDRInfoReq);
@@ -589,15 +603,16 @@ ProcXvMCGetDRInfo(ClientPtr client)
      * segment she prepared for us.
      */
 
-    rep.isLocal = 0;
-
-    if ( NULL != (patternP = (CARD32 *)shmat( stuff->shmKey, NULL, SHM_RDONLY ))) {
-	register volatile CARD32 *patternC = patternP;
+    rep.isLocal = 1;
+#ifdef HAS_XVMCSHM
+    patternP = (CARD32 *)shmat( stuff->shmKey, NULL, SHM_RDONLY );
+    if ( -1 != (long) patternP) {
+        register volatile CARD32 *patternC = patternP;
 	register int i;
 	CARD32 magic = stuff->magic;
 	
 	rep.isLocal = 1;
-	i = getpagesize() / sizeof(CARD32);
+	i = 1024 / sizeof(CARD32);
 	
 	while ( i-- ) {
 	    if (*patternC++ != magic) {
@@ -606,9 +621,10 @@ ProcXvMCGetDRInfo(ClientPtr client)
 	    }
 	    magic = ~magic;
 	}
-	shmdt( (char *)patternP );
+	shmdt( (char *)patternP ); 
     }
-
+#endif /* HAS_XVMCSHM */
+    
     WriteToClient(client, sizeof(xvmcGetDRInfoReply), 
 		  (char*)&rep);
     if (rep.length) {      

X11Libre/xserver

commit b4ac8b26677ed443fa4663c7c2b273632dae01a9

Xext: xvmc: drop dead SHM code path
Xvmc isn't using SHM for over 20 years now. There's still a code path for it
that could be enabled explicitly by manually setting HAS_XVMCSHM, but no
indication whatsoever that this ever has been done.

Considering that Xvmc in general already is obsolete for very long time now,
we can safely assume this code path is really dead and can be removed.

Fixes: 9a26d6f39e199bad287d4e538ef75700a0102788
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

Xext/xvmc.c
@@ -23,12 +23,6 @@
 #include "servermd.h"
 #include "xvmcext.h"
 
-#ifdef HAS_XVMCSHM
-#include <sys/ipc.h>
-#include <sys/types.h>
-#include <sys/shm.h>
-#endif                          /* HAS_XVMCSHM */
-
 #define SERVER_XVMC_MAJOR_VERSION               1
 #define SERVER_XVMC_MINOR_VERSION               1
 
@@ -609,10 +603,6 @@ ProcXvMCGetDRInfo(ClientPtr client)
     ScreenPtr pScreen;
     XvMCScreenPtr pScreenPriv;
 
-#ifdef HAS_XVMCSHM
-    volatile CARD32 *patternP;
-#endif
-
     REQUEST(xvmcGetDRInfoReq);
     REQUEST_SIZE_MATCH(xvmcGetDRInfoReq);
 
@@ -649,28 +639,6 @@ ProcXvMCGetDRInfo(ClientPtr client)
      * Read back to the client what she has put in the shared memory
      * segment she prepared for us.
      */
-
-#ifdef HAS_XVMCSHM
-    patternP = (CARD32 *) shmat(stuff->shmKey, NULL, SHM_RDONLY);
-    if (-1 != (long) patternP) {
-        volatile CARD32 *patternC = patternP;
-        int i;
-        CARD32 magic = stuff->magic;
-
-        reply.isLocal = 1;
-        i = 1024 / sizeof(CARD32);
-
-        while (i--) {
-            if (*patternC++ != magic) {
-                reply.isLocal = 0;
-                break;
-            }
-            magic = ~magic;
-        }
-        shmdt((char *) patternP);
-    }
-#endif                          /* HAS_XVMCSHM */
-
     WriteToClient(client, sizeof(xvmcGetDRInfoReply), &reply);
     WriteToClient(client, buflen, buf);
     free(buf);