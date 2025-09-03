d11a7ef4597ce5c611ed66bc632848285b689a4b
Tautvis <gtautvis@gmail.com>
2025-09-03 20:09:48 +0300

    Patch 2/5 xfree86: libseat support, wireup libseat
    Refactor libseat code for Xlibre and add to meson.build:
    
    * update meson for seat-libseat
    * refactor code:
      * seatd-libseat.c - add aditional private include headers
      * make seatd_libseat_init to accept keeptty state as parameter
      * include xf86Events, xf86_priv - make visible xf86VTLeave for seatd code (it
        is neccesary because seatd can force console leave)
    
    Signed-off-By: Tautvis <gtautvis@gmail.com>

--- hw/xfree86/os-support/shared/seatd-libseat.c
+++ hw/xfree86/os-support/shared/seatd-libseat.c
@@ -36,12 +36,16 @@
 #include <libseat.h>
 
 #include "os.h"
-#include "linux.h"
 #include "xf86.h"
+#include "xf86_priv.h"
 #include "xf86platformBus.h"
 #include "xf86Xinput.h"
+#include "xf86Xinput_priv.h"
 #include "xf86Priv.h"
 #include "globals.h"
+
+#include "config/hotplug_priv.h"
+
 #include "seatd-libseat.h"
 
 // ============ libseat client adapter ======================
@@ -160,7 +164,7 @@ event_handler(int fd, int ready, void *data)
 /*
  * Handle libseat logging.
  */
-static void
+static _X_ATTRIBUTE_PRINTF(2, 0)  void
 log_libseat(enum libseat_log_level level, const char *fmt, va_list args)
 {
     MessageType xmt;
@@ -192,6 +196,8 @@ log_libseat(enum libseat_log_level level, const char *fmt, va_list args)
 /*
  * Initialise the libseat client.
  *
+ * @param KeepTty_state - the KeepTty parameter value
+ *
  * Returns:
  *   0 if all ok
  *   1 if not possible
@@ -199,10 +205,10 @@ log_libseat(enum libseat_log_level level, const char *fmt, va_list args)
  *   -EPIPE (-32) if the seat opening failed.
  */
 int
-seatd_libseat_init(void)
+seatd_libseat_init(BOOL KeepTty_state)
 {
-    if (!ServerIsNotSeat0() && xf86HasTTYs() && linux_parse_vt_settings(TRUE) && !linux_get_keeptty()) {
-        LogMessage(X_INFO,
+    if (!ServerIsNotSeat0() && xf86HasTTYs() && !KeepTty_state) {
+        LogMessage(X_WARNING,
             "seat-libseat: libseat integration requires -keeptty which "
             "was not provided, disabling\n");
         return 1;
@@ -304,7 +310,7 @@ check_duplicate_device(int maj, int min) {
 void
 seatd_libseat_open_device(InputInfoPtr p, int *pfd, Bool *paused)
 {
-    int id, fd;
+    int id = -1, fd = -1;
     char *path = xf86CheckStrOption(p->options, "Device", NULL);
 
     if (!libseat_active()) {
