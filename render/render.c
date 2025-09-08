fecb7c2325fbeb75bb2686d995d004d076bbde11
Enrico Weigelt, metux IT consult <info@metux.net>
2025-09-08 17:06:38 +0200

    render: consolidate byte-swapping in ProcRenderCreateConicalGradient()
    No need for extra functions and call tables for the few trivial lines.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- render/render.c
+++ render/render.c
@@ -96,8 +96,6 @@ static int ProcRenderCreateConicalGradient(ClientPtr pClient);
 
 static int ProcRenderDispatch(ClientPtr pClient);
 
-static int SProcRenderCreateConicalGradient(ClientPtr pClient);
-
 static int SProcRenderDispatch(ClientPtr pClient);
 
 int (*ProcRenderVector[RenderNumberRequests]) (ClientPtr) = {
@@ -175,7 +173,7 @@ int (*SProcRenderVector[RenderNumberRequests]) (ClientPtr) = {
         ProcRenderCreateSolidFill,
         ProcRenderCreateLinearGradient,
         ProcRenderCreateRadialGradient,
-        SProcRenderCreateConicalGradient};
+        ProcRenderCreateConicalGradient};
 
 int RenderErrBase;
 static DevPrivateKeyRec RenderClientPrivateKeyRec;
@@ -1872,17 +1870,14 @@ SingleRenderCreateRadialGradient(ClientPtr client,
 }
 
 static int
-SingleRenderCreateConicalGradient(ClientPtr client)
+SingleRenderCreateConicalGradient(ClientPtr client,
+                                  xRenderCreateConicalGradientReq *stuff)
 {
     PicturePtr pPicture;
     int len;
     int error = 0;
     xFixed *stops;
     xRenderColor *colors;
-
-    REQUEST(xRenderCreateConicalGradientReq);
-
-    REQUEST_AT_LEAST_SIZE(xRenderCreateConicalGradientReq);
 
     LEGAL_NEW_RESOURCE(stuff->pid, client);
 
@@ -1938,31 +1933,6 @@ swapStops(void *stuff, int num)
         swaps(colors);
         ++colors;
     }
-}
-
-static int _X_COLD
-SProcRenderCreateConicalGradient(ClientPtr client)
-{
-    int len;
-
-    REQUEST(xRenderCreateConicalGradientReq);
-    REQUEST_AT_LEAST_SIZE(xRenderCreateConicalGradientReq);
-
-    swapl(&stuff->pid);
-    swapl(&stuff->center.x);
-    swapl(&stuff->center.y);
-    swapl(&stuff->angle);
-    swapl(&stuff->nStops);
-
-    len = (client->req_len << 2) - sizeof(xRenderCreateConicalGradientReq);
-    if (stuff->nStops > UINT32_MAX / (sizeof(xFixed) + sizeof(xRenderColor)))
-        return BadLength;
-    if (len != stuff->nStops * (sizeof(xFixed) + sizeof(xRenderColor)))
-        return BadLength;
-
-    swapStops(stuff + 1, stuff->nStops);
-
-    return ProcRenderCreateConicalGradient(client);
 }
 
 static int _X_COLD
@@ -2584,13 +2554,11 @@ PanoramiXRenderCreateRadialGradient(ClientPtr client,
 }
 
 static int
-PanoramiXRenderCreateConicalGradient(ClientPtr client)
+PanoramiXRenderCreateConicalGradient(ClientPtr client,
+                                     xRenderCreateConicalGradientReq *stuff)
 {
-    REQUEST(xRenderCreateConicalGradientReq);
     PanoramiXRes *newPict;
     int result = Success;
-
-    REQUEST_AT_LEAST_SIZE(xRenderCreateConicalGradientReq);
 
     if (!(newPict = calloc(1, sizeof(PanoramiXRes))))
         return BadAlloc;
@@ -2601,7 +2569,7 @@ PanoramiXRenderCreateConicalGradient(ClientPtr client)
 
     XINERAMA_FOR_EACH_SCREEN_BACKWARD({
         stuff->pid = newPict->info[walkScreenIdx].id;
-        result = SingleRenderCreateConicalGradient(client);
+        result = SingleRenderCreateConicalGradient(client, stuff);
         if (result != Success)
             break;
     });
@@ -3104,10 +3072,29 @@ ProcRenderCreateRadialGradient(ClientPtr client)
 static int
 ProcRenderCreateConicalGradient(ClientPtr client)
 {
+    REQUEST(xRenderCreateConicalGradientReq);
+    REQUEST_AT_LEAST_SIZE(xRenderCreateConicalGradientReq);
+
+    if (client->swapped) {
+        swapl(&stuff->pid);
+        swapl(&stuff->center.x);
+        swapl(&stuff->center.y);
+        swapl(&stuff->angle);
+        swapl(&stuff->nStops);
+
+        int len = (client->req_len << 2) - sizeof(xRenderCreateConicalGradientReq);
+        if (stuff->nStops > UINT32_MAX / (sizeof(xFixed) + sizeof(xRenderColor)))
+            return BadLength;
+        if (len != stuff->nStops * (sizeof(xFixed) + sizeof(xRenderColor)))
+            return BadLength;
+
+        swapStops(stuff + 1, stuff->nStops);
+    }
+
 #ifdef XINERAMA
-    return (usePanoramiX ? PanoramiXRenderCreateConicalGradient(client)
-                         : SingleRenderCreateConicalGradient(client));
+    return (usePanoramiX ? PanoramiXRenderCreateConicalGradient(client, stuff)
+                         : SingleRenderCreateConicalGradient(client, stuff));
 #else
-    return SingleRenderCreateConicalGradient(client);
+    return SingleRenderCreateConicalGradient(client, stuff);
 #endif
 }
