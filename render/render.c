b319a66e55b57980a74af49440231c439861de28
Enrico Weigelt, metux IT consult <info@metux.net>
2025-09-08 13:41:27 +0200

    render: consolidate byte-swapping in ProcRenderFreeGlyphs()
    No need for extra functions and call tables for the few trivial lines.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- render/render.c
+++ render/render.c
@@ -103,7 +103,6 @@ static int SProcRenderTrapezoids(ClientPtr pClient);
 static int SProcRenderTriangles(ClientPtr pClient);
 static int SProcRenderTriStrip(ClientPtr pClient);
 static int SProcRenderTriFan(ClientPtr pClient);
-static int SProcRenderFreeGlyphs(ClientPtr pClient);
 static int SProcRenderCompositeGlyphs(ClientPtr pClient);
 static int SProcRenderFillRectangles(ClientPtr pClient);
 static int SProcRenderSetPictureTransform(ClientPtr pClient);
@@ -177,7 +176,7 @@ int (*SProcRenderVector[RenderNumberRequests]) (ClientPtr) = {
         ProcRenderFreeGlyphSet,
         ProcRenderAddGlyphs,
         _not_implemented, /* SProcRenderAddGlyphsFromPicture */
-        SProcRenderFreeGlyphs,
+        ProcRenderFreeGlyphs,
         SProcRenderCompositeGlyphs,
         SProcRenderCompositeGlyphs,
         SProcRenderCompositeGlyphs,
@@ -1154,12 +1153,18 @@ static int
 ProcRenderFreeGlyphs(ClientPtr client)
 {
     REQUEST(xRenderFreeGlyphsReq);
+    REQUEST_AT_LEAST_SIZE(xRenderFreeGlyphsReq);
+
+    if (client->swapped) {
+        swapl(&stuff->glyphset);
+        SwapRestL(stuff);
+    }
+
     GlyphSetPtr glyphSet;
     int rc, nglyph;
     CARD32 *gids;
     CARD32 glyph;
 
-    REQUEST_AT_LEAST_SIZE(xRenderFreeGlyphsReq);
     rc = dixLookupResourceByType((void **) &glyphSet, stuff->glyphset,
                                  GlyphSetType, client, DixRemoveAccess);
     if (rc != Success) {
@@ -2057,16 +2062,6 @@ SProcRenderTriFan(ClientPtr client)
     swaps(&stuff->ySrc);
     SwapRestL(stuff);
     return ProcRenderTriFan(client);
-}
-
-static int _X_COLD
-SProcRenderFreeGlyphs(ClientPtr client)
-{
-    REQUEST(xRenderFreeGlyphsReq);
-    REQUEST_AT_LEAST_SIZE(xRenderFreeGlyphsReq);
-    swapl(&stuff->glyphset);
-    SwapRestL(stuff);
-    return ProcRenderFreeGlyphs(client);
 }
 
 static int _X_COLD
