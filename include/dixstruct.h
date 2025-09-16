405bb58d956a5397c82507831aa6912540005cd7
Enrico Weigelt, metux IT consult <info@metux.net>
2025-09-16 13:14:39 +0200

    dix: use xorg_list saveSet list
    Simplify saveSet handling by just using trivial `struct xorg_list` instead
    of complicated dynamically resized array. Common operations like insert or
    remove are much simpler using lists instead of arrays.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- include/dixstruct.h
+++ include/dixstruct.h
@@ -59,19 +59,9 @@ typedef enum { ClientStateInitial,
     ClientStateGone
 } ClientState;
 
-typedef struct _saveSet {
-    struct _Window *windowPtr;
-    Bool toRoot;
-    Bool map;
-} SaveSetElt;
-#define SaveSetWindow(ss)   ((ss).windowPtr)
-#define SaveSetToRoot(ss)   ((ss).toRoot)
-#define SaveSetShouldMap(ss)	    ((ss).map)
-#define SaveSetAssignWindow(ss,w)   ((ss).windowPtr = (w))
-#define SaveSetAssignToRoot(ss,tr)  ((ss).toRoot = (tr))
-#define SaveSetAssignMap(ss,m)      ((ss).map = (m))
-
 struct _Client {
+    /* changes in here must be done with great caution, as long as some
+       external drivers might still depend on specific layout */
     void *requestBuffer;
     void *osPrivate;             /* for OS layer, including scheduler */
     struct xorg_list ready;      /* List of clients ready to run */
@@ -92,8 +82,10 @@ struct _Client {
     XID errorValue;
     int sequence;
     int ignoreCount;            /* count for Attend/IgnoreClient */
-    unsigned numSaved;          /* amount of windows in saveSet */
-    SaveSetElt *saveSet;
+
+    int __dummy0;                       /* used to be numSave */
+    void *__dummy1;                     /* used to be saveSet */
+
     int (**requestVector) (ClientPtr /* pClient */ );
     CARD32 req_len;             /* length of current request */
     unsigned int replyBytesRemaining;
@@ -109,6 +101,10 @@ struct _Client {
     DeviceIntPtr clientPtr;
     struct _ClientId *clientIds;
     int req_fds;
+
+    /* driver should NEVER EVER touch anything beyond here */
+
+    struct xorg_list saveSets;
 };
 
 extern _X_EXPORT TimeStamp currentTime;
