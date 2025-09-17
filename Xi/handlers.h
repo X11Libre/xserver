81ef29b4467cedd2fa01221a46a70450aa72d965
Enrico Weigelt, metux IT consult <info@metux.net>
2025-09-17 12:25:35 +0200

    Xi: inline SProcXSelectExtensionEvent()
    No need to have a hole bunch of extra functions, if we can just easily
    inline the few relevant lines.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xi/handlers.h
+++ Xi/handlers.h
@@ -96,7 +96,6 @@ int SProcXISetClientPointer(ClientPtr client);
 int SProcXISetFocus(ClientPtr client);
 int SProcXIUngrabDevice(ClientPtr client);
 int SProcXIWarpPointer(ClientPtr client);
-int SProcXSelectExtensionEvent(ClientPtr client);
 int SProcXSendExtensionEvent(ClientPtr client);
 int SProcXSetDeviceFocus(ClientPtr client);
 int SProcXUngrabDeviceButton(ClientPtr client);
