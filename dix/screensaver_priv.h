6441ad5f33facdcc670788569484d3c56945ed43
Enrico Weigelt, metux IT consult <info@metux.net>
2025-10-01 13:32:22 +0200

    WIP

--- dix/screensaver_priv.h
+++ dix/screensaver_priv.h
@@ -8,10 +8,24 @@
 #include <X11/Xdefs.h>
 #include <X11/Xmd.h>
 
+#include "include/callback.h"
+#include "include/dix.h"
+#include "include/screenint.h"
+
 extern CARD32 defaultScreenSaverTime;
 extern CARD32 defaultScreenSaverInterval;
 extern CARD32 ScreenSaverTime;
 extern CARD32 ScreenSaverInterval;
 extern Bool screenSaverSuspended;
+
+extern CallbackListPtr ScreenSaverCallback;
+
+/* XACE_SCREENSAVER_ACCESS */
+typedef struct {
+    ClientPtr client;
+    ScreenPtr screen;
+    Mask access_mode;
+    int status;
+} ScreenSaverAccessCallbackParam;
 
 #endif /* _XSERVER_DIX_SCREENSAVER_PRIV_H */
