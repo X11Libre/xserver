f5498831830eae9a66b0f3181439bf2269cf0d22
notbabaisyou <though-went-some-simple@proton.me>
2025-10-24 09:58:50 +0200

    modesetting: Omit DRM_FORMAT_MOD_INVALID.
    Previously it was possible for the invalid modifier to be placed in the IN_FORMATS or IN_FORMATS_ASYNC arrays, this is of course not wanted as this can cause problems with devices that lack support for explicit modifiers.

--- hw/xfree86/drivers/video/modesetting/drmmode_display.c
+++ hw/xfree86/drivers/video/modesetting/drmmode_display.c
@@ -2428,7 +2428,11 @@ populate_format_modifiers(xf86CrtcPtr crtc, const drmModePlane *kplane,
 
             if ((i < mod->offset) || (i > mod->offset + 63))
                 continue;
+
             if (!(mod->formats & (1 << (i - mod->offset))))
+                continue;
+
+            if (mod->modifier == DRM_FORMAT_MOD_INVALID)
                 continue;
 
             num_modifiers++;
