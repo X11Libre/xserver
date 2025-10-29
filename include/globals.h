1016de0af9c0b56d9c8eecace07fe93de7064993
Enrico Weigelt, metux IT consult <info@metux.net>
2025-10-29 13:49:36 +0100

    os: utils: unexport PanoramiXExtensionDisabledHack
    This variable is only used in os layer and PanoramiX, nowhere else,
    and shouldn't be visible to drivers at all.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- include/globals.h
+++ include/globals.h
@@ -18,8 +18,4 @@ extern _X_EXPORT int defaultColorVisualClass;
 extern _X_EXPORT int GrabInProgress;
 extern _X_EXPORT char *SeatId;
 
-#ifdef XINERAMA
-extern _X_EXPORT Bool PanoramiXExtensionDisabledHack;
-#endif /* XINERAMA */
-
 #endif                          /* !_XSERV_GLOBAL_H_ */
