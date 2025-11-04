4b46c4235675d81980a33a1d00b29ea39adda169
Enrico Weigelt, metux IT consult <info@metux.net>
2025-11-04 16:30:42 +0100

    [PR #1327] dix: add dixAnyOtherGrabbed() and unexport GrabInProgress
    PR: https://github.com/X11Libre/xserver/pull/1327

--- dix/dispatch.c
+++ dix/dispatch.c
@@ -4268,3 +4268,9 @@ DetachOffloadGPU(ScreenPtr secondary)
     secondary->is_offload_secondary = FALSE;
 }
 
+bool dixAnyOtherGrabbed(ClientPtr client)
+{
+    return ((grabState == GrabActive) &&
+            (grabClient != NULL) &&
+            (grabClient != client));
+}
