03c89d1417a1f9f5c8e34cd75101fe640ced50ec
Enrico Weigelt, metux IT consult <info@metux.net>
2025-11-07 14:29:35 +0100

    dix: unexport defaultColorVisualClass
    Not used by any drivers, so no need to keep it exported.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/dix_priv.h
+++ dix/dix_priv.h
@@ -68,6 +68,8 @@ extern _X_EXPORT Bool enableIndirectGLX;
 
 extern const char *defaultFontPath;
 
+extern int defaultColorVisualClass;
+
 /*
  * @brief callback right after one screen's root window has been initialized
  *
