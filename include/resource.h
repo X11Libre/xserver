96cc3f14a291a1bde7791d7a7aa4cfa446df090f
Enrico Weigelt, metux IT consult <info@metux.net>
2025-11-10 19:29:54 +0100

    dix: unexport lastResourceType and TypeMask
    Not used by any drivers, so no need to keep them exported
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- include/resource.h
+++ include/resource.h
@@ -229,9 +229,6 @@ extern _X_EXPORT int dixLookupResourceByClass(void **result,
                                               ClientPtr client,
                                               Mask access_mode);
 
-extern _X_EXPORT RESTYPE lastResourceType;
-extern _X_EXPORT RESTYPE TypeMask;
-
 /*
  * @brief allocate a XID (resource ID) for the server itself
  *
