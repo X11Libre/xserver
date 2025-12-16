804a0d324b58d5168137a366a707f6d5dd135356
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-16 16:46:06 +0100

    xfree86: input: mouse: use version information from server
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/drivers/input/mouse/src/mouse.c
+++ hw/xfree86/drivers/input/mouse/src/mouse.c
@@ -70,6 +70,8 @@
 #include <sys/ioctl.h>
 #endif
 
+#include "xorgVersion.h"
+
 #include "compiler.h"
 
 #include "xisb.h"
@@ -3731,7 +3733,9 @@ static XF86ModuleVersionInfo xf86MouseVersionRec =
     MODINFOSTRING1,
     MODINFOSTRING2,
     XORG_VERSION_CURRENT,
-    PACKAGE_VERSION_MAJOR, PACKAGE_VERSION_MINOR, PACKAGE_VERSION_PATCHLEVEL,
+    XORG_VERSION_MAJOR,
+    XORG_VERSION_MINOR,
+    XORG_VERSION_PATCH,
     ABI_CLASS_XINPUT,
     ABI_XINPUT_VERSION,
     MOD_CLASS_XINPUT,
