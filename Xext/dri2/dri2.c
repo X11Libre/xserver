c0ce51f2704ad8f3601e226cf208624f8b72bf3a
Enrico Weigelt, metux IT consult <info@metux.net>
2026-02-06 13:05:31 +0100

    [PR #1967] dri2: declare variables where needed in DRI2GetMSC()
    PR: https://github.com/X11Libre/xserver/pull/1967

--- Xext/dri2/dri2.c
+++ Xext/dri2/dri2.c
@@ -1199,10 +1199,8 @@ DRI2GetMSC(DrawablePtr pDraw, CARD64 * ust, CARD64 * msc, CARD64 * sbc)
 {
     ScreenPtr pScreen = pDraw->pScreen;
     DRI2ScreenPtr ds = DRI2GetScreen(pDraw->pScreen);
-    DRI2DrawablePtr pPriv;
-    Bool ret;
 
-    pPriv = DRI2GetDrawable(pDraw);
+    DRI2DrawablePtr pPriv = DRI2GetDrawable(pDraw);
     if (pPriv == NULL) {
         xf86DrvMsg(pScreen->myNum, X_ERROR,
                    "[DRI2] %s: bad drawable\n", __func__);
@@ -1221,8 +1219,7 @@ DRI2GetMSC(DrawablePtr pDraw, CARD64 * ust, CARD64 * msc, CARD64 * sbc)
      * drawables
      */
 
-    ret = (*ds->GetMSC) (pDraw, ust, msc);
-    if (!ret)
+    if (!(ds->GetMSC(pDraw, ust, msc)))
         return BadDrawable;
 
     *sbc = pPriv->swap_count;
@@ -1235,23 +1232,17 @@ DRI2WaitMSC(ClientPtr client, DrawablePtr pDraw, CARD64 target_msc,
             CARD64 divisor, CARD64 remainder)
 {
     DRI2ScreenPtr ds = DRI2GetScreen(pDraw->pScreen);
-    DRI2DrawablePtr pPriv;
-    Bool ret;
-
-    pPriv = DRI2GetDrawable(pDraw);
+    DRI2DrawablePtr pPriv = DRI2GetDrawable(pDraw);
     if (pPriv == NULL)
         return BadDrawable;
 
     /* Old DDX just completes immediately */
     if (!ds->ScheduleWaitMSC) {
         DRI2WaitMSCComplete(client, pDraw, target_msc, 0, 0);
-
         return Success;
     }
 
-    ret =
-        (*ds->ScheduleWaitMSC) (client, pDraw, target_msc, divisor, remainder);
-    if (!ret)
+    if (!(ds->ScheduleWaitMSC(client, pDraw, target_msc, divisor, remainder)))
         return BadDrawable;
 
     return Success;
@@ -1260,9 +1251,7 @@ DRI2WaitMSC(ClientPtr client, DrawablePtr pDraw, CARD64 target_msc,
 int
 DRI2WaitSBC(ClientPtr client, DrawablePtr pDraw, CARD64 target_sbc)
 {
-    DRI2DrawablePtr pPriv;
-
-    pPriv = DRI2GetDrawable(pDraw);
+    DRI2DrawablePtr pPriv = DRI2GetDrawable(pDraw);
     if (pPriv == NULL)
         return BadDrawable;
 
