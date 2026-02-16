d58bcde60b08cee71c113b4ee1fcb8262827d82c
Enrico Weigelt, metux IT consult <info@metux.net>
2026-02-16 13:17:58 +0100

    .github: update XTS
    use commit 6cf94400a09abecd6b86e4eb6441741acecd51f6
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- .github/scripts/install-prereq.sh
+++ .github/scripts/install-prereq.sh
@@ -15,7 +15,7 @@ build_meson   libxcvt           $(fdo_mirror libxcvt)                      libxc
 build_meson   xorgproto         $(fdo_mirror xorgproto)                    xorgproto-2024.1
 
 # really must be build via autoconf instead of meson, otherwise piglit wont find the test programs
-build_ac_xts  xts               $(fdo_mirror xts)                          aae51229af810efba24412511f60602fab53eded
+build_ac_xts  xts               $(fdo_mirror xts)                          6cf94400a09abecd6b86e4eb6441741acecd51f6
 
 clone_source piglit             $(fdo_mirror piglit)                       59111996534f875ca88bce51f21fa2e6564895da
 
