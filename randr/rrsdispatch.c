127935eddcec76ca413f252324e7dcec147c5bb1
Peter Hutterer <peter.hutterer@who-t.net>
2026-04-17 18:12:14 +1000

    [PR #2372] randr, Xext: remove stale length swaps
    PR: https://github.com/X11Libre/xserver/pull/2372

--- randr/rrsdispatch.c
+++ randr/rrsdispatch.c
@@ -117,7 +117,6 @@ SProcRRGetScreenResourcesCurrent(ClientPtr client)
     REQUEST(xRRGetScreenResourcesCurrentReq);
 
     REQUEST_SIZE_MATCH(xRRGetScreenResourcesCurrentReq);
-    swaps(&stuff->length);
     swapl(&stuff->window);
     return ProcRRGetScreenResourcesCurrent(client);
 }
