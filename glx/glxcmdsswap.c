3fb4bd0c7dc9e7d68b7fcd1116e283c54105a28e
Peter Hutterer <peter.hutterer@who-t.net>
2026-04-17 10:33:52 +1000

    [PR #2364] glx: fix wrong pointer passed to non-swap handlers in TexImage/CopySubBuffer
    PR: https://github.com/X11Libre/xserver/pull/2364

--- glx/glxcmdsswap.c
+++ glx/glxcmdsswap.c
@@ -637,7 +637,7 @@ __glXDispSwap_BindTexImageEXT(__GLXclientState * cl, GLbyte * pc)
     __GLX_SWAP_INT(buffer);
     __GLX_SWAP_INT(num_attribs);
 
-    return __glXDisp_BindTexImageEXT(cl, (GLbyte *) pc);
+    return __glXDisp_BindTexImageEXT(cl, (GLbyte *) req);
 }
 
 int
@@ -662,7 +662,7 @@ __glXDispSwap_ReleaseTexImageEXT(__GLXclientState * cl, GLbyte * pc)
     __GLX_SWAP_INT(drawId);
     __GLX_SWAP_INT(buffer);
 
-    return __glXDisp_ReleaseTexImageEXT(cl, (GLbyte *) pc);
+    return __glXDisp_ReleaseTexImageEXT(cl, (GLbyte *) req);
 }
 
 int
@@ -690,7 +690,7 @@ __glXDispSwap_CopySubBufferMESA(__GLXclientState * cl, GLbyte * pc)
     __GLX_SWAP_INT(pc + 12);
     __GLX_SWAP_INT(pc + 16);
 
-    return __glXDisp_CopySubBufferMESA(cl, pc);
+    return __glXDisp_CopySubBufferMESA(cl, (GLbyte *) req);
 
 }
 
