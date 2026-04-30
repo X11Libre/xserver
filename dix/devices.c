93f08560703b84b3c1f924c95255d1351f0e263a
Enrico Weigelt, metux IT consult <info@metux.net>
2026-04-30 16:52:26 +0200

    dix: devices: fix missing free in AddInputDevice()
    When dixCallDeviceAccessCallback() rejects, we need to really clean up.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/devices.c
+++ dix/devices.c
@@ -302,6 +302,7 @@ AddInputDevice(ClientPtr client, DeviceProc deviceProc, Bool autoStart)
      */
     if (dixCallDeviceAccessCallback(client, dev, DixCreateAccess)) {
         dixFreePrivates(dev->devPrivates, PRIVATE_DEVICE);
+        free(dev->deviceGrab.sync.event);
         free(dev);
         return NULL;
     }
