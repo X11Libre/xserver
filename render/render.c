X11Libre/xserver

commit fe9fcfe98f3a72c787ae961b2b48b886f5957af6

render: ProcRenderQueryPictFormats(): use x_rpcbuf_t
Use x_rpcbuf_t for reply payload assembly and X_SEND_REPLY_WITH_RPCBUF()
for sending it all out.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

render/render.c
@@ -303,7 +303,6 @@ ProcRenderQueryPictFormats(ClientPtr client)
     xPictScreen *pictScreen;
     xPictDepth *pictDepth;
     xPictVisual *pictVisual;
-    xPictFormInfo *pictForm;
     CARD32 *pictSubpixel;
     VisualPtr pVisual;
     DepthPtr pDepth;
@@ -350,25 +349,16 @@ ProcRenderQueryPictFormats(ClientPtr client)
     else
         numSubpixel = numScreens;
 
-    rlength = (sizeof(xRenderQueryPictFormatsReply) +
-               nformat * sizeof(xPictFormInfo) +
+    rlength = (nformat * sizeof(xPictFormInfo) +
                numScreens * sizeof(xPictScreen) +
                ndepth * sizeof(xPictDepth) +
                nvisual * sizeof(xPictVisual) + numSubpixel * sizeof(CARD32));
 
-    xRenderQueryPictFormatsReply *reply = calloc(1, rlength);
-    if (!reply)
-        return BadAlloc;
-    reply->type = X_Reply;
-    reply->sequenceNumber = client->sequence;
-    reply->length = bytes_to_int32(rlength - sizeof(xGenericReply));
-    reply->numFormats = nformat;
-    reply->numScreens = numScreens;
-    reply->numDepths = ndepth;
-    reply->numVisuals = nvisual;
-    reply->numSubpixel = numSubpixel;
+    x_rpcbuf_t rpcbuf = { .swapped = client->swapped, .err_clear = TRUE };
 
-    pictForm = (xPictFormInfo *) (reply + 1);
+    xPictFormInfo *pictForm = x_rpcbuf_reserve(&rpcbuf, rlength);
+    if (!pictForm)
+        return BadAlloc;
 
     for (unsigned int walkScreenIdx = 0; walkScreenIdx < numScreens; walkScreenIdx++) {
         ScreenPtr walkScreen = screenInfo.screens[walkScreenIdx];
@@ -470,18 +460,23 @@ ProcRenderQueryPictFormats(ClientPtr client)
         ++pictSubpixel;
     }
 
+    xRenderQueryPictFormatsReply reply = {
+        .numFormats = nformat,
+        .numScreens = numScreens,
+        .numDepths = ndepth,
+        .numVisuals = nvisual,
+        .numSubpixel = numSubpixel,
+    };
+
     if (client->swapped) {
-        swaps(&reply->sequenceNumber);
-        swapl(&reply->length);
-        swapl(&reply->numFormats);
-        swapl(&reply->numScreens);
-        swapl(&reply->numDepths);
-        swapl(&reply->numVisuals);
-        swapl(&reply->numSubpixel);
+        swapl(&reply.numFormats);
+        swapl(&reply.numScreens);
+        swapl(&reply.numDepths);
+        swapl(&reply.numVisuals);
+        swapl(&reply.numSubpixel);
     }
-    WriteToClient(client, rlength, reply);
-    free(reply);
-    return Success;
+
+    return X_SEND_REPLY_WITH_RPCBUF(client, reply, rpcbuf);
 }
 
 static int

commit fb3e8db9849449c74df2614f5471f3cc60612299

render: zero reply buffers in QueryPictFormats and QueryFilters
ProcRenderQueryPictFormats left the internal padding of xPictFormInfo (pad1)
and xPictDepth (pad1, pad2) unwritten, and ProcRenderQueryFilters left the
odd-count alias alignment slot and the filter-name alignment tail unwritten.
Both reply buffers were allocated with the non-zeroing x_rpcbuf_reserve(), so
those bytes disclosed uninitialized server heap to the client.

Use x_rpcbuf_reserve0() for both reply payloads.

Fixes: fe9fcfe98f3a ("render: ProcRenderQueryPictFormats(): use x_rpcbuf_t")
Fixes: 81f93d19a02a ("render: ProcRenderQueryFilters(): use x_rpcbuf_t")
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

render/render.c
@@ -244,7 +244,7 @@ ProcRenderQueryPictFormats(ClientPtr client)
 
     x_rpcbuf_t rpcbuf = { .swapped = client->swapped, .err_clear = TRUE };
 
-    xPictFormInfo *pictForm = x_rpcbuf_reserve(&rpcbuf, rlength);
+    xPictFormInfo *pictForm = x_rpcbuf_reserve0(&rpcbuf, rlength);
     if (!pictForm)
         return BadAlloc;
 
@@ -1531,7 +1531,7 @@ ProcRenderQueryFilters(ClientPtr client)
     total_bytes = (len << 2);
 
     x_rpcbuf_t rpcbuf = { .swapped = client->swapped, .err_clear = TRUE };
-    aliases = (INT16 *) x_rpcbuf_reserve(&rpcbuf, total_bytes);
+    aliases = (INT16 *) x_rpcbuf_reserve0(&rpcbuf, total_bytes);
     if (!aliases)
         return BadAlloc;
 