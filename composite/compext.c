249afa0b6f3898d5935bef70400d9c062a196553
Xinhao Liu <liuxinhao@kylinsec.com.cn>
2026-07-13 11:01:08 +0800

    composite: Fix PanoramiX overlay window release
    PanoramiX overlay resources contain physical window IDs for each
    screen. Look them up as regular window resources when releasing the
    client's overlay reference.
    
    Signed-off-by: Xinhao Liu <liuxinhao@kylinsec.com.cn>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2259>

--- composite/compext.c
+++ composite/compext.c
@@ -908,8 +908,8 @@ ProcCompositeReleaseOverlayWindow(ClientPtr client)
 
     FOR_NSCREENS_BACKWARD(i) {
         if ((rc = dixLookupResourceByType((void **) &pWin, win->info[i].id,
-                                          XRT_WINDOW, client,
-                                          DixUnknownAccess))) {
+                                          X11_RESTYPE_WINDOW, client,
+                                          DixGetAttrAccess))) {
             client->errorValue = stuff->window;
             return rc;
         }
