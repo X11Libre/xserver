90d9425c474ecfbd5c7be7a51865667b012d7ab5
Enrico Weigelt, metux IT consult <info@metux.net>
2026-07-22 16:43:13 +0200

    Xext: composite: make compAllocPixmap() bool
    use stdbool's bool type instead of our own.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/composite/compositeext_priv.h
+++ Xext/composite/compositeext_priv.h
@@ -1,7 +1,6 @@
-/* SPDX-License-Identifier: MIT OR X11
+/* SPDX-License-Identifier: MIT OR X11 OR AGPLv3+
  *
- * Copyright © 1987, 1998  The Open Group
- * Copyright © 2024 Enrico Weigelt, metux IT consult <info@metux.net>
+ * Copyright © 2026 Enrico Weigelt, metux IT consult <info@metux.net>
  */
 #ifndef _XSERVER_COMPOSITEEXT_PRIV_H_
 #define _XSERVER_COMPOSITEEXT_PRIV_H_
