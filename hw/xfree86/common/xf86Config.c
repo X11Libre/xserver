9c831ce80bb32c001d84c488676d689198a7f8bb
Enrico Weigelt, metux IT consult <info@metux.net>
2026-07-24 19:26:38 +0200

    xfree86: use stdbool's 'bool' instead of 'Bool' for local variables
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/common/xf86Config.c
+++ hw/xfree86/common/xf86Config.c
@@ -43,6 +43,7 @@
  */
 #include <xorg-config.h>
 
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <grp.h>
@@ -266,7 +267,7 @@ xf86ModulelistFromConfig(void ***optlist)
     };
     void **optarray;
     XF86LoadPtr modp;
-    Bool found;
+    bool found;
 
     /*
      * make sure the config file has been parsed and that we have a
@@ -532,7 +533,7 @@ static void
 configFiles(XF86ConfFilesPtr fileconf)
 {
     MessageType pathFrom;
-    Bool must_copy;
+    bool must_copy;
     int size, countDirs;
     char *temp_path, *log_buf, *start, *end;
 
@@ -1055,7 +1056,7 @@ static Bool
 checkCoreInputDevices(serverLayoutPtr servlayoutp, Bool implicitLayout)
 {
     InputInfoPtr corePointer = NULL, coreKeyboard = NULL;
-    Bool foundPointer = FALSE, foundKeyboard = FALSE;
+    bool foundPointer = FALSE, foundKeyboard = FALSE;
     const char *pointerMsg = NULL, *keyboardMsg = NULL;
     InputInfoPtr *devs,         /* iterator */
      indp;
@@ -1710,7 +1711,7 @@ configScreen(confScreenPtr screenp, XF86ConfScreenPtr conf_screen, int scrnum,
     int count = 0;
     XF86ConfDisplayPtr dispptr;
     XF86ConfAdaptorLinkPtr conf_adaptor;
-    Bool defaultMonitor = FALSE;
+    bool defaultMonitor = FALSE;
     XF86ConfScreenRec local_conf_screen;
     int i;
 
@@ -2187,7 +2188,7 @@ configExtensions(XF86ConfExtensionsPtr conf_ext)
             char *name = xf86OptionName(o);
             char *val = xf86OptionValue(o);
             char *n;
-            Bool enable = TRUE;
+            bool enable = TRUE;
 
             /* Handle "No<ExtensionName>" */
             n = xf86NormalizeName(name);
@@ -2332,7 +2333,7 @@ xf86HandleConfigFile(Bool autoconfig)
     const char *scanptr;
     Bool singlecard = 0;
 #endif
-    Bool implicit_layout = FALSE;
+    bool implicit_layout = FALSE;
     XF86ConfLayoutPtr layout;
 
     if (!autoconfig) {
