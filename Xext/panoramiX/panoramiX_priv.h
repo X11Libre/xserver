dd4a8ba9bd94ca3c319062ebe896df94d0ffe366
Enrico Weigelt, metux IT consult <info@metux.net>
2026-07-24 17:16:22 +0200

    [PR #3399] Xext: panoramix: add PanoramiXIsMasterScreen() / PanoramiXIsSlaveScreen()
    Adding inline helper functions for detection of whether a given Screen
    is a master or slave screen.
    
    With PanoramiX enabled, the first one (->myNum == 0) is considered the
    master, while the others are slaves. When disabled, all screens are
    considered master.
    
    That's allowing us to move some more implementation detail logic out of
    DDX/DIX into the extension code and get rid of some ifdef's.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    PR: https://github.com/X11Libre/xserver/pull/3399

--- Xext/panoramiX/panoramiX_priv.h
+++ Xext/panoramiX/panoramiX_priv.h
@@ -8,6 +8,8 @@
 #include <stdbool.h>
 
 #include "include/extinit.h"
+#include "include/screenint.h"
+#include "include/scrnintstr.h"
 
 /*
  * @brief return TRUE if panoramiX / xinerama extension is enabled
@@ -25,6 +27,29 @@ static inline bool PanoramiXIsEnabled(void) {
  */
 static inline bool PanoramiXIsDisabled(void) {
     return !PanoramiXIsEnabled();
+}
+
+/*
+ * @brief return TRUE if panoramix enabled and screen is the master or
+ * panoramix is disabled
+ *
+ * this function is usually called in order to check whether the given
+ * screen behaves normally (eg. sending events, etc) and is NOT a
+ * panoramix slave screen.
+ */
+static inline bool PanoramiXIsMasterScreen(ScreenPtr pScreen) {
+    return PanoramiXIsDisabled() || (pScreen->myNum == 0);
+}
+
+/*
+ * @brief return TRUE if panoramix enabled and screen is a slave.
+ *
+ * this function is usually called in order to check whether the given
+ * screen is a panoramix slave screen (thus shall not be visible to
+ * clients, eg. sending events, etc)
+ */
+static inline bool PanoramiXIsSlaveScreen(ScreenPtr pScreen) {
+    return PanoramiXIsDisabled() || (pScreen->myNum == 0);
 }
 
 #endif /* _XSERVER_PANORAMIX_PRIV_H_ */
