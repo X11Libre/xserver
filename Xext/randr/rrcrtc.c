02409dc507625114556c95938c04ff7b46fc4e1f
Enrico Weigelt, metux IT consult <info@metux.net>
2026-07-24 19:47:08 +0200

    xext: randr: use stdbool's 'bool' instead of 'Bool' for local variables
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/randr/rrcrtc.c
+++ Xext/randr/rrcrtc.c
@@ -22,6 +22,7 @@
  */
 #include <dix-config.h>
 
+#include <stdbool.h>
 #include <X11/Xatom.h>
 
 #include "dix/dix_priv.h"
@@ -350,7 +351,7 @@ static void
 RRComputeContiguity(ScreenPtr pScreen)
 {
     rrScrPriv(pScreen);
-    Bool discontiguous = TRUE;
+    bool discontiguous = TRUE;
     int i, n = pScrPriv->numCrtcs;
 
     int *reachable = calloc(n, sizeof(int));
@@ -755,9 +756,9 @@ RRCrtcSet(RRCrtcPtr crtc,
           int y, Rotation rotation, int numOutputs, RROutputPtr * outputs)
 {
     ScreenPtr pScreen = crtc->pScreen;
-    Bool ret = FALSE;
-    Bool recompute = TRUE;
-    Bool crtcChanged;
+    bool ret = FALSE;
+    bool recompute = TRUE;
+    bool crtcChanged;
     int  o;
 
     BUG_RETURN_VAL(numOutputs != 0 && outputs == NULL, FALSE);
@@ -938,7 +939,7 @@ RRCrtcDestroyResource(void *value, XID pid)
 Bool
 RRCrtcGammaSet(RRCrtcPtr crtc, CARD16 *red, CARD16 *green, CARD16 *blue)
 {
-    Bool ret = TRUE;
+    bool ret = TRUE;
 
 #if RANDR_12_INTERFACE
     ScreenPtr pScreen = crtc->pScreen;
@@ -964,7 +965,7 @@ RRCrtcGammaSet(RRCrtcPtr crtc, CARD16 *red, CARD16 *green, CARD16 *blue)
 static Bool
 RRCrtcGammaGet(RRCrtcPtr crtc)
 {
-    Bool ret = TRUE;
+    bool ret = TRUE;
 
 #if RANDR_12_INTERFACE
     ScreenPtr pScreen = crtc->pScreen;
@@ -1868,7 +1869,7 @@ RRConstrainCursorHarder(DeviceIntPtr pDev, ScreenPtr pScreen, int mode, int *x,
                         int *y)
 {
     rrScrPriv(pScreen);
-    Bool ret;
+    bool ret;
     ScreenPtr secondary;
 
     /* intentional dead space -> let it float */
@@ -1908,7 +1909,7 @@ Bool
 RRReplaceScanoutPixmap(DrawablePtr pDrawable, PixmapPtr pPixmap, Bool enable)
 {
     rrScrPriv(pDrawable->pScreen);
-    Bool ret = TRUE;
+    bool ret = TRUE;
     PixmapPtr *saved_scanout_pixmap;
     int i;
 
@@ -1918,7 +1919,7 @@ RRReplaceScanoutPixmap(DrawablePtr pDrawable, PixmapPtr pPixmap, Bool enable)
 
     for (i = 0; i < pScrPriv->numCrtcs; i++) {
         RRCrtcPtr crtc = pScrPriv->crtcs[i];
-        Bool size_fits;
+        bool size_fits;
 
         saved_scanout_pixmap[i] = crtc->scanout_pixmap;
 
