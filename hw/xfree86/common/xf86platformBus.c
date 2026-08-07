X11Libre/xserver

commit 58f140059be93b4e27ebfcc60e820c7a9f399084

xfree86: use stdbool's 'bool' instead of 'Bool' for local variables
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

hw/xfree86/common/xf86platformBus.c
@@ -28,6 +28,8 @@
 #include <xorg-config.h>
 
 #ifdef XSERVER_PLATFORM_BUS
+
+#include <stdbool.h>
 #include <assert.h>
 #include <errno.h>
 
@@ -253,7 +255,7 @@ int
 xf86platformProbe(void)
 {
     int i;
-    Bool pci = TRUE;
+    bool pci = TRUE;
     XF86ConfOutputClassPtr cl, cl_head = (xf86configptr) ?
             xf86configptr->conf_outputclass_lst : NULL;
 
@@ -516,7 +518,7 @@ xf86UnclaimPlatformSlot(struct xf86_platform_device *d, GDevPtr dev)
 static Bool doPlatformProbe(struct xf86_platform_device *dev, DriverPtr drvp,
                             GDevPtr gdev, int flags, intptr_t match_data)
 {
-    Bool foundScreen = FALSE;
+    bool foundScreen = FALSE;
     int entity;
 
     entity = xf86ClaimPlatformSlot(dev, drvp, 0,
@@ -562,7 +564,7 @@ static Bool
 probeSingleDevice(struct xf86_platform_device *dev, DriverPtr drvp, GDevPtr gdev, int flags)
 {
     int k;
-    Bool foundScreen = FALSE;
+    bool foundScreen = FALSE;
     struct pci_device *pPci;
     const struct pci_id_match *const devices = drvp->supported_devices;
 
@@ -603,7 +605,7 @@ isGPUDevice(GDevPtr gdev)
 int
 xf86platformProbeDev(DriverPtr drvp)
 {
-    Bool foundScreen = FALSE;
+    bool foundScreen = FALSE;
     GDevPtr *devList;
     const unsigned numDevs = xf86MatchDevice(drvp->driverName, &devList);
     int i, j;
@@ -672,7 +674,7 @@ xf86platformProbeDev(DriverPtr drvp)
 int
 xf86platformAddGPUDevices(DriverPtr drvp)
 {
-    Bool foundScreen = FALSE;
+    bool foundScreen = FALSE;
     GDevPtr *devList;
     int j;
 
@@ -817,7 +819,7 @@ xf86platformRemoveDevice(int index)
 {
     EntityPtr entity;
     int ent_num, i, j, scrnum;
-    Bool found;
+    bool found;
 
     for (ent_num = 0; ent_num < xf86NumEntities; ent_num++) {
         entity = xf86Entities[ent_num];

hw/xfree86/os-support/shared/drm_platform.c
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <string.h>
 
 #include "config/hotplug_priv.h"
@@ -127,7 +128,7 @@ xf86PlatformDeviceCheckBusID(struct xf86_platform_device *device, const char *bu
 void
 xf86PlatformReprobeDevice(int index, struct OdevAttributes *attribs)
 {
-    Bool ret;
+    bool ret;
     char *dpath = attribs->path;
 
     ret = get_drm_info(attribs, dpath, index);
@@ -145,7 +146,7 @@ xf86PlatformDeviceProbe(struct OdevAttributes *attribs)
 {
     int i;
     char *path = attribs->path;
-    Bool ret;
+    bool ret;
 
     if (!path)
         goto out_free;

hw/xfree86/common/xf86Bus.c
@@ -32,6 +32,7 @@
 
 #include <assert.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <X11/X.h>
@@ -77,7 +78,7 @@ BusRec primaryBus = { BUS_NONE, {0} };
 Bool
 xf86CallDriverProbe(DriverPtr drv, Bool detect_only)
 {
-    Bool foundScreen = FALSE;
+    bool foundScreen = FALSE;
 
 #ifdef XSERVER_PLATFORM_BUS
     /* xf86platformBus.c does not support Xorg -configure */

hw/xfree86/common/xf86Config.c
@@ -43,6 +43,7 @@
  */
 #include <xorg-config.h>
 
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <grp.h>
@@ -263,7 +264,7 @@ xf86ModulelistFromConfig(void ***optlist)
     };
     void **optarray;
     XF86LoadPtr modp;
-    Bool found;
+    bool found;
 
     /*
      * make sure the config file has been parsed and that we have a
@@ -529,7 +530,7 @@ static void
 configFiles(XF86ConfFilesPtr fileconf)
 {
     MessageType pathFrom;
-    Bool must_copy;
+    bool must_copy;
     int size, countDirs;
     char *temp_path, *log_buf, *start, *end;
 
@@ -1052,7 +1053,7 @@ static Bool
 checkCoreInputDevices(serverLayoutPtr servlayoutp, Bool implicitLayout)
 {
     InputInfoPtr corePointer = NULL, coreKeyboard = NULL;
-    Bool foundPointer = FALSE, foundKeyboard = FALSE;
+    bool foundPointer = FALSE, foundKeyboard = FALSE;
     const char *pointerMsg = NULL, *keyboardMsg = NULL;
     InputInfoPtr *devs,         /* iterator */
      indp;
@@ -1707,7 +1708,7 @@ configScreen(confScreenPtr screenp, XF86ConfScreenPtr conf_screen, int scrnum,
     int count = 0;
     XF86ConfDisplayPtr dispptr;
     XF86ConfAdaptorLinkPtr conf_adaptor;
-    Bool defaultMonitor = FALSE;
+    bool defaultMonitor = FALSE;
     XF86ConfScreenRec local_conf_screen;
     int i;
 
@@ -2184,7 +2185,7 @@ configExtensions(XF86ConfExtensionsPtr conf_ext)
             char *name = xf86OptionName(o);
             char *val = xf86OptionValue(o);
             char *n;
-            Bool enable = TRUE;
+            bool enable = TRUE;
 
             /* Handle "No<ExtensionName>" */
             n = xf86NormalizeName(name);
@@ -2329,7 +2330,7 @@ xf86HandleConfigFile(Bool autoconfig)
     const char *scanptr;
     Bool singlecard = 0;
 #endif
-    Bool implicit_layout = FALSE;
+    bool implicit_layout = FALSE;
     XF86ConfLayoutPtr layout;
 
     if (!autoconfig) {

hw/xfree86/common/xf86Configure.c
@@ -26,6 +26,7 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <stdbool.h>
 
 #include "include/misc.h"
 #include "os/ddx_priv.h"
@@ -642,7 +643,7 @@ DoConfigure(void)
 
     /* Call all of the probe functions, reporting the results. */
     for (CurrentDriver = 0; CurrentDriver < xf86NumDrivers; CurrentDriver++) {
-        Bool found_screen;
+        bool found_screen;
         DriverRec *const drv = xf86DriverList[CurrentDriver];
 
         found_screen = xf86CallDriverProbe(drv, TRUE);
@@ -742,7 +743,7 @@ DoConfigure(void)
             k = screennum > 0 ? screennum : 1;
             for (l = oldNumScreens; l < xf86NumScreens; l++) {
                 /* is screen primary? */
-                Bool primary = FALSE;
+                bool primary = FALSE;
 
                 for (n = 0; n < xf86Screens[l]->numEntities; n++) {
                     if (xf86IsEntityPrimary(xf86Screens[l]->entityList[n])) {

hw/xfree86/common/xf86Cursor.c
@@ -26,6 +26,7 @@
  */
 #include <xorg-config.h>
 
+#include <stdbool.h>
 #include <X11/X.h>
 #include <X11/Xmd.h>
 #include <X11/extensions/XIproto.h>
@@ -135,7 +136,7 @@ xf86SetViewport(ScreenPtr pScreen, int x, int y)
 static void
 xf86PointerMoved(ScrnInfoPtr pScr, int x, int y)
 {
-    Bool frameChanged = FALSE;
+    bool frameChanged = FALSE;
 
     /*
      * check whether (x,y) belongs to the visual part of the screen
@@ -192,7 +193,7 @@ xf86SwitchMode(ScreenPtr pScreen, DisplayModePtr mode)
 {
     ScrnInfoPtr pScr = xf86ScreenToScrn(pScreen);
     ScreenPtr pCursorScreen;
-    Bool Switched;
+    bool Switched;
     int px, py;
     DeviceIntPtr dev, it;
 
@@ -569,7 +570,7 @@ xf86InitOrigins(void)
     int x1, x2, y1, y2, left, right, top, bottom;
     int i, j, ref, minX, minY, min, max;
     xf86ScreenLayoutPtr pLayout;
-    Bool OldStyleConfig = FALSE;
+    bool OldStyleConfig = FALSE;
 
     memset(xf86ScreenLayout, 0, MAXSCREENS * sizeof(xf86ScreenLayoutRec));
 

hw/xfree86/common/xf86DGA.c
@@ -38,6 +38,7 @@
  */
 #include <xorg-config.h>
 
+#include <stdbool.h>
 #include <assert.h>
 #include <string.h>
 #include <X11/X.h>
@@ -99,7 +100,7 @@ static int DGAEventBase;
     dixLookupPrivate(&(pScreen)->devPrivates, &DGAScreenKeyRec))
 
 typedef struct _FakedVisualList {
-    Bool free;
+    bool free;
     VisualPtr pVisual;
     struct _FakedVisualList *next;
 } FakedVisualList;
@@ -119,8 +120,8 @@ typedef struct {
     FakedVisualList *fakedVisuals;
     ColormapPtr dgaColormap;
     ColormapPtr savedColormap;
-    Bool grabMouse;
-    Bool grabKeyboard;
+    bool grabMouse;
+    bool grabKeyboard;
 } DGAScreenRec, *DGAScreenPtr;
 
 Bool
@@ -402,7 +403,7 @@ xf86SetDGAMode(ScrnInfoPtr pScrn, int num, DGADevicePtr devRet)
         return BadAlloc;
 
     if (!pScreenPriv->current) {
-        Bool oldVTSema = pScrn->vtSema;
+        bool oldVTSema = !!pScrn->vtSema;
 
         pScrn->vtSema = FALSE;  /* kludge until we rewrite VT switching */
         (*pScrn->EnableDisableFBAccess) (pScrn, FALSE);

hw/xfree86/common/xf86DPMS.c
@@ -29,7 +29,9 @@
  */
 #include <xorg-config.h>
 
+#include <stdbool.h>
 #include <X11/X.h>
+
 #include "os.h"
 #include "globals.h"
 #include "windowstr.h"
@@ -67,7 +69,7 @@ xf86DPMSInit(ScreenPtr pScreen, DPMSSetProcPtr set, int flags)
     ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
     void *DPMSOpt;
     MessageType enabled_from = X_DEFAULT;
-    Bool enabled = TRUE;
+    bool enabled = TRUE;
 
     DPMSOpt = xf86FindOption(pScrn->options, "dpms");
     if (DPMSDisabledSwitch) {

commit 463d3dd0293b0646d47d84f6a1c44b5af8bc2edb

xfree86: platform bus: return bool from xf86platformAddDevice()
xf86platformAddDevice() only ever returns 0 (success) or -1 (failure),
so switch its signature to bool. This is the proper fix for the broken
error path introduced by commit 58f140059b (xfree86: use stdbool's
'bool' instead of 'Bool' for local variables), which changed the local
'ret' in xf86PlatformReprobeDevice() to bool while the function still
returned int: comparing bool against -1 is always false, so the failure
path silently became dead code, and gcc's -Werror=bool-compare turned
it into a hard build error on the CI lanes.

Instead of reverting the local variable to int (the pragmatic one-line
fix), make the API itself bool-typed: return TRUE on success, FALSE on
error, and adapt both callers to use !ret. This keeps the bool sweep
consistent and prevents the same trap for future callers.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

hw/xfree86/common/xf86platformBus.c
@@ -719,7 +719,7 @@ xf86PlatformFindHotplugDriver(int dev_index)
     return hp_driver;
 }
 
-int
+bool
 xf86platformAddDevice(const char *driver_name, int index)
 {
     int i, old_screens, scr_index, scrnum;
@@ -727,7 +727,7 @@ xf86platformAddDevice(const char *driver_name, int index)
     screenLayoutPtr layout;
 
     if (!xf86Info.autoAddGPU)
-        return -1;
+        return FALSE;
 
     /* Load modesetting driver if no driver given, or driver open failed */
     if (!driver_name || !xf86LoadOneModule(driver_name, NULL)) {
@@ -747,14 +747,14 @@ xf86platformAddDevice(const char *driver_name, int index)
 
     if (!drvp) {
         ErrorF("can't find driver %s for hotplugged device\n", driver_name);
-        return -1;
+        return FALSE;
     }
 
     old_screens = xf86NumGPUScreens;
     doPlatformProbe(&xf86_platform_devices[index], drvp, NULL,
                     PLATFORM_PROBE_GPU_SCREEN, 0);
     if (old_screens == xf86NumGPUScreens)
-        return -1;
+        return FALSE;
     i = old_screens;
 
     for (layout = xf86ConfigLayout.screens; layout->screen != NULL;
@@ -770,7 +770,7 @@ xf86platformAddDevice(const char *driver_name, int index)
     if (!xf86GPUScreens[i]->configured) {
         ErrorF("hotplugged device %d didn't configure\n", i);
         xf86DeleteScreen(xf86GPUScreens[i]);
-        return -1;
+        return FALSE;
     }
 
    /*
@@ -787,7 +787,7 @@ xf86platformAddDevice(const char *driver_name, int index)
        xf86DeleteScreen(xf86GPUScreens[i]);
        xf86UnclaimPlatformSlot(&xf86_platform_devices[index], NULL);
        xf86NumGPUScreens = old_screens;
-       return -1;
+       return FALSE;
    }
    dixSetPrivate(&xf86GPUScreens[i]->pScreen->devPrivates,
                  xf86ScreenKey, xf86GPUScreens[i]);
@@ -799,7 +799,7 @@ xf86platformAddDevice(const char *driver_name, int index)
        xf86DeleteScreen(xf86GPUScreens[i]);
        xf86UnclaimPlatformSlot(&xf86_platform_devices[index], NULL);
        xf86NumGPUScreens = old_screens;
-       return -1;
+       return FALSE;
    }
    /* attach unbound to the configured protocol screen (or 0) */
    scrnum = xf86GPUScreens[i]->confScreen->screennum;
@@ -811,7 +811,7 @@ xf86platformAddDevice(const char *driver_name, int index)
    RRResourcesChanged(xf86Screens[scrnum]->pScreen);
    RRTellChanged(xf86Screens[scrnum]->pScreen);
 
-   return 0;
+   return TRUE;
 }
 
 void

hw/xfree86/common/xf86platformBus_priv.h
@@ -36,7 +36,7 @@ int xf86_add_platform_device(struct OdevAttributes *attribs, Bool unowned);
 int xf86_remove_platform_device(int dev_index);
 Bool xf86_get_platform_device_unowned(int index);
 
-int xf86platformAddDevice(const char *driver_name, int index);
+bool xf86platformAddDevice(const char *driver_name, int index);
 void xf86platformRemoveDevice(int index);
 
 void xf86platformVTProbe(void);

hw/xfree86/os-support/shared/drm_platform.c
@@ -137,7 +137,7 @@ xf86PlatformReprobeDevice(int index, struct OdevAttributes *attribs)
         return;
     }
     ret = xf86platformAddDevice(xf86PlatformFindHotplugDriver(index), index);
-    if (ret == -1)
+    if (!ret)
         xf86_remove_platform_device(index);
 }
 
@@ -183,7 +183,7 @@ out_free:
 void NewGPUDeviceRequest(struct OdevAttributes *attribs)
 {
     int old_num = xf86_num_platform_devices;
-    int ret;
+    bool ret;
     const char *driver_name;
 
     xf86PlatformDeviceProbe(attribs);
@@ -199,7 +199,7 @@ void NewGPUDeviceRequest(struct OdevAttributes *attribs)
     driver_name = xf86PlatformFindHotplugDriver(xf86_num_platform_devices - 1);
 
     ret = xf86platformAddDevice(driver_name, xf86_num_platform_devices-1);
-    if (ret == -1)
+    if (!ret)
         xf86_remove_platform_device(xf86_num_platform_devices-1);
 
     ErrorF("xf86: found device %d\n", xf86_num_platform_devices);