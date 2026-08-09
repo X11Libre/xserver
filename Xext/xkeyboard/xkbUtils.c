ddf7c27899157fdaa7dd498e0d2894b4608b16e4
Alan Coopersmith <alan.coopersmith@oracle.com>
2026-08-09 09:55:00 -0700

    [PR #3551] xkb: Fix -Wcalloc-transposed-args warning in _XkbCopyGeom()
    Reported by gcc 16.1:
    xkb/xkbUtils.c:1440:39: warning: ‘calloc’ sizes specified with ‘sizeof’ in
    the earlier argument and not in the later argument [-Wcalloc-transposed-args]
     1440 |             dst->geom = calloc(sizeof(XkbGeometryRec), 1);
          |                                       ^~~~~~~~~~~~~~
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2272>
    PR: https://github.com/X11Libre/xserver/pull/3551

--- Xext/xkeyboard/xkbUtils.c
+++ Xext/xkeyboard/xkbUtils.c
@@ -1435,7 +1435,7 @@ _XkbCopyGeom(XkbDescPtr src, XkbDescPtr dst)
     /* geometry */
     if (src->geom) {
         if (!dst->geom) {
-            dst->geom = calloc(sizeof(XkbGeometryRec), 1);
+            dst->geom = calloc(1, sizeof(XkbGeometryRec));
             if (!dst->geom)
                 return FALSE;
         }
