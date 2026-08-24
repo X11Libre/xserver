X11Libre/xserver

commit 2a41522349f250f09a8feb512889d38e5bc08ed8

os: make FormatInt64() handle LONG_MIN correctly
When compiling with gcc 15.2.0 using -O3 -m64 on Solaris SPARC & x64,
we'd get a test failure of:

Assertion failed: strcmp(logmsg, expected) == 0,
 file ../test/signal-logging.c, line 339, function logging_format

because 'num *= -1' produced a value that was out of the range of the
int64_t it was being stored in.  (Compiling with -O2 worked fine with
the same compiler/configuration/platform though.)

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/2117>

os/fmt.c
@@ -16,12 +16,14 @@
 void
 FormatInt64(int64_t num, char *string)
 {
+    uint64_t unum = num;
+
     if (num < 0) {
         string[0] = '-';
-        num *= -1;
+        unum = num * -1;
         string++;
     }
-    FormatUInt64(num, string);
+    FormatUInt64(unum, string);
 }
 
 /* Format a number into a string in a signal safe manner. The string should be

commit 78c46d0a912880735e50c43895f28eb5142d59f6

os: fix undefined behavior in FormatInt64() for INT64_MIN
'num * -1' is undefined behavior when num is INT64_MIN, since the
positive value (9223372036854775808) exceeds INT64_MAX.

Fix by casting to uint64_t before negation, which is well-defined
unsigned modular arithmetic and produces the correct absolute value
for all int64_t values including INT64_MIN.

Fixes: https://github.com/X11Libre/xserver/issues/3610
Fixes: 2a41522349f250f09a8feb512889d38e5bc08ed8
Reported-by: Alex <alexzk1@users.noreply.github.com>
Signed-off-by: Defiant <defiant@x11libre.dev>
PR: https://github.com/X11Libre/xserver/pull/3614

os/fmt.c
@@ -20,7 +20,7 @@ FormatInt64(int64_t num, char *string)
 
     if (num < 0) {
         string[0] = '-';
-        unum = num * -1;
+        unum = -(uint64_t)num;
         string++;
     }
     FormatUInt64(unum, string);