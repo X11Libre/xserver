30ba72539bfda633a93a4ff79c24e091c7aefba1
Enrico Weigelt, metux IT consult <info@metux.net>
2026-08-31 14:06:26 +0200

    dix/events: enumerate all EventType cases in CheckMotion
    CheckMotion updates the sprite coordinates only for pointer events
    (button, motion, touch). All remaining EventType values return FALSE and are
    now listed explicitly so a -Wswitch-enum build confirms every value is
    consciously considered.
    
    No behaviour change.

--- dix/events.c
+++ dix/events.c
@@ -3224,7 +3224,36 @@ CheckMotion(DeviceEvent *ev, DeviceIntPtr pDev)
         case ET_TouchUpdate:
         case ET_TouchEnd:
             break;
-        default:
+        case ET_KeyPress:
+        case ET_KeyRelease:
+        case ET_TouchOwnership:
+        case ET_Enter:
+        case ET_Leave:
+        case ET_FocusIn:
+        case ET_FocusOut:
+        case ET_ProximityIn:
+        case ET_ProximityOut:
+        case ET_DeviceChanged:
+        case ET_Hierarchy:
+        case ET_DGAEvent:
+        case ET_RawKeyPress:
+        case ET_RawKeyRelease:
+        case ET_RawButtonPress:
+        case ET_RawButtonRelease:
+        case ET_RawMotion:
+        case ET_RawTouchBegin:
+        case ET_RawTouchUpdate:
+        case ET_RawTouchEnd:
+        case ET_XQuartz:
+        case ET_BarrierHit:
+        case ET_BarrierLeave:
+        case ET_GesturePinchBegin:
+        case ET_GesturePinchUpdate:
+        case ET_GesturePinchEnd:
+        case ET_GestureSwipeBegin:
+        case ET_GestureSwipeUpdate:
+        case ET_GestureSwipeEnd:
+        case ET_Internal:
             /* all other events return FALSE */
             return FALSE;
         }
